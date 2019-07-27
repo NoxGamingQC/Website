73ab6e3d58f884cccad8c04665e8ad79322c9f22
NoxGamingQC <noxracinggaming@gmail.com>
2019-07-27 00:01:21 -0400

    Implement layout for views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,81 +1,21 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
+    <head>
+        <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>NoxGamingQC</title>
+
+        <title>NoxGamingQC - @yield('title')</title>
         <link rel="icon" href="/img/Avatar.png" type="image/png">
         <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="/css/font-awesome.min.css">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
+    </head>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="/home" style="display: flex; alignItems: center">
-                        <img src="/img/Avatar.png" alt="NoxGamingQC" width="70px" height="60px" style="padding: 7px 14px" >NoxGamingQC
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        <li className="nav-home"><a href="/home"><i class="fa fa-home" aria-hidden="true"></i> Welcome <span class="sr-only">current</span></a></li>
-                        <li class="nav-stream"><a href="/stream"><i class="fa fa-video-camera" aria-hidden="true"></i> Stream</a></li>
-                        <li class="nav-partnership"><a href="/partnership"><i class="fa fa-handshake-o" aria-hidden="true"></i> Affiliates|Partners</a></li>
-                        <li class="nav-positivity_stream_team"><a href="/positivity_stream_team"><i class="fa fa-heart" aria-hidden="true"></i> Positivity Stream Team</a></li>
-                        <li class="nav-noxbot"><a href="/noxbot"><i class="fa fa-user" aria-hidden="true"></i> NoxBOT</a></li>
-                        <li class="nav-contact"><a href="/contact"><i class="fa fa-address-book " aria-hidden="true"></i> Contact me</a></li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-login"><a href="{{ route('login') }}">Login</a></li>
-                            <li class="nav-register"><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
+    <body>
+        <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
+        @include('layouts.navbar')
         @yield('content')
-    </div>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
+        @include('layouts.footer')
+        <script src="{{mix('js/app.js')}}"></script>
+    </body>
 </html>
--- resources/views/profile_show.blade.php
+++ resources/views/profile_show.blade.php
@@ -1,109 +1,50 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>NoxGamingQC - Profile</title>
-    <link rel="icon" href="/img/Avatar.png" type="image/png">
-    <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="/css/font-awesome.min.css">
-</head>
-
-<body>
-    <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
-    <!-- NAVBAR -->
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="/home" style="display: flex; alignItems: center">
-                    NoxGamingQC
-                </a>
-            </div>
-            <div class="collapse navbar-collapse" id="bs-navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="nav-home"><a href="/home"><i class="fa fa-home" aria-hidden="true"></i> Welcome <span class="sr-only">current</span></a></li>
-                    <li class="nav-stream"><a href="/stream"><i class="fa fa-video-camera" aria-hidden="true"></i> Stream</a></li>
-                    <li class="nav-partnership"><a href="/partnership"><i class="fa fa-handshake-o" aria-hidden="true"></i> Affiliates|Partners</a></li>
-                    <li class="nav-positivity_stream_team"><a href="/positivity_stream_team"><i class="fa fa-heart" aria-hidden="true"></i> Positivity Stream Team</a></li>
-                    <li class="nav-noxbot"><a href="/noxbot"><i class="fa fa-user" aria-hidden="true"></i> NoxBOT</a></li>
-                    <li class="nav-contact"><a href="/contact"><i class="fa fa-address-book " aria-hidden="true"></i> Contact me</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <!-- ///////////////////////////////////////////////////////////// -->
-    <!-- BODY -->
-
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-primary">
-                <div class="panel-body">
-                    <div class="text-center">
-                        <img class="img-circle" src="{{$avatarURL}}" alt="{{$discordName}}" width="120px" style="padding: 7px 14px" />
-                        <h1>{{$username}} <small>{{$firstname}} {{$lastname}}</small></h1>
-                        <h3>{{$grade}}</h3>
-                        @foreach ($discordBadges as $badge)
-                        <img src="/img/{{$badge}}.png" alt="{{$badge}}" width="75px" style="padding: 7px 14px" />
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="panel panel-primary">
-                <div class="panel-body">
-                    <h4>User aknowledgement</h4>
-                    <hr />
-                    <ul>
-                        <li><b>Account Level:</b> {{$grade}}</li>
-                        <li><b>Premium User:</b> {{$isPremium}}</li>
-                        @if($gender)<li><b>Gender:</b> {{$gender}}</li>@endif
-                        @if($birthdate)<li><b>Birthdate:</b> {{$birthdate}}</li>@endif
-                        @if($age)<li><b>Age:</b> {{$age}}</li>@endif
-                        <li><b>Country:</b> {{$country}}</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="panel panel-primary">
-                <div class="panel-body">
-                    <h4>Discord user aknowledgement</h4>
-                    <hr />
-                    <ul>
-                        <li><b>ID:</b> {{$discordID}}</li>
-                        <li><b>Username:</b> {{$discordName . '#' . $discriminator}}</li>
-                        <li><b>Language:</b> {{$language}}</li>
-                        <li><b>Nitro Subscription:</b> {{$nitroSubscription}}</li>
-                    </ul>
+@extends('layouts.app')
+@section('title', $username . '\'s profile')
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-primary">
+            <div class="panel-body">
+                <div class="text-center">
+                    <img class="img-circle" src="{{$avatarURL}}" alt="{{$discordName}}" width="120px" style="padding: 7px 14px" />
+                    <h1>{{$username}} <small>{{$firstname}} {{$lastname}}</small></h1>
+                    <h3>{{$grade}}</h3>
+                    @foreach ($discordBadges as $badge)
+                    <img src="/img/{{$badge}}.png" alt="{{$badge}}" width="75px" style="padding: 7px 14px" />
+                    @endforeach
                 </div>
             </div>
         </div>
     </div>
-
-    <!-- ///////////////////////////////////////////////////////////// -->
-    <!-- FOOTER -->
-    <div class="row">
-        <div class="col-md-12 text-center">
-            <hr />
-            <p>© Copyright Nox's Studios. All right reserved.</p>
+    <div class="col-md-6">
+        <div class="panel panel-primary">
+            <div class="panel-body">
+                <h4>User aknowledgement</h4>
+                <hr />
+                <ul>
+                    <li><b>Account Level:</b> {{$grade}}</li>
+                    <li><b>Premium User:</b> {{$isPremium}}</li>
+                    @if($gender)<li><b>Gender:</b> {{$gender}}</li>@endif
+                    @if($birthdate)<li><b>Birthdate:</b> {{$birthdate}}</li>@endif
+                    @if($age)<li><b>Age:</b> {{$age}}</li>@endif
+                    <li><b>Country:</b> {{$country}}</li>
+                </ul>
+            </div>
         </div>
     </div>
-    <!-- ///////////////////////////////////////////////////////////// -->
-    <script src="{{mix('js/app.js')}}"></script>
-</body>
-
-</html>
+    <div class="col-md-6">
+        <div class="panel panel-primary">
+            <div class="panel-body">
+                <h4>Discord user aknowledgement</h4>
+                <hr />
+                <ul>
+                    <li><b>ID:</b> {{$discordID}}</li>
+                    <li><b>Username:</b> {{$discordName . '#' . $discriminator}}</li>
+                    <li><b>Language:</b> {{$language}}</li>
+                    <li><b>Nitro Subscription:</b> {{$nitroSubscription}}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
