2b71b6a09aa848ed299335ace452a56deee2fd93
NoxGamingQC <noxracinggaming@gmail.com>
2019-07-27 01:30:26 -0400

    Adding french language file

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Les informations que vous avez saisi sont incorrect..',
+    'throttle' => 'Trop d\'essai manqué. Réessayez dans :seconds secondes.',
+
+];
