eb48ca4feb42a9bed9a01fdbdc408330992c06e9
NoxGamingQC <noxracinggaming@gmail.com>
2019-07-27 00:56:22 -0400

    Remove useless pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,26 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>NoxGamingQC - @yield('title')</title>
-        <link rel="icon" href="/img/Avatar.png" type="image/png">
-        <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="/css/font-awesome.min.css">
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @if(env('APP_ENV', 'developement'))
+    <title>Dev - {{env('APP_NAME')}} - @yield('title')</title>
+    @else
+    <title>{{env('APP_NAME')}} - @yield('title')</title>
+    @endif
+    <link rel="icon" href="/img/Avatar.png" type="image/png">
+    <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+</head>
 
-    <body>
-        <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
-        @include('layouts.navbar')
-        @yield('content')
-        @include('layouts.footer')
-        <script src="{{mix('js/app.js')}}"></script>
-    </body>
+<body>
+    <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
+    @include('layouts.navbar')
+    @yield('content')
+    @include('layouts.footer')
+    <script src="{{mix('js/app.js')}}"></script>
+</body>
+
 </html>
