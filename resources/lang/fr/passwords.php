a5a04aa55ac98b83c2803dbf6523c2226270ccea
NoxGamingQC <noxracinggaming@gmail.com>
2019-07-27 01:30:26 -0400

    Adding french language file

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Le mot de passe doit contenir au moins six caractère et correspondre à la confirmation.',
+    'reset' => 'Votre mot de passe à bien été réinitialisé!',
+    'sent' => 'Un courriel contenant votre lien pour réinitialisé votre mot de passe vous à été envoyé!',
+    'token' => 'Ce jeton de réinitialisation de mot de passe est invalide.',
+    'user' => "Nous n'avons pas trouvé d'utilisateur ayant cette addresse courriel.",
+
+];
