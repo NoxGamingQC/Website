648fb824bef7175cffaab6e1cfa8a33781a63b56
NoxGamingQC <noxracinggaming@gmail.com>
2019-07-30 23:56:26 -0400

    Add locales

---
+++ resources/lang/en/generic.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'copyright' => "© Copyright Nox's Studios. All right reserved.",
+    'welcome' => "Welcome",
+    'streams' => "Streams",
+    'projects' => "Projects",
+    'contact_us' => "Contact us"
+];
--- resources/lang/en/profile.php
+++ resources/lang/en/profile.php
@@ -3,5 +3,15 @@
 return [
     'administrator' => "Administrator",
     'moderator' => "Moderator",
-    'developper' => "Developper"
+    'developper' => "Developper",
+    'user_acknowledgement' => "User Acknowledgement",
+    'discord_user_acknowledgement' => "Discord User Acknowledgement",
+    'account_level' => "Account Level",
+    'premium_user' => "Premium User",
+    'gender' => "Gender",
+    'country' => "Country",
+    'id' => "ID",
+    'username' => "Username",
+    'language' => "Language",
+    'nitro_subscription' => "Nitro subscription"
 ];
