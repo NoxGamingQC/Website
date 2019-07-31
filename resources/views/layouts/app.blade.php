c813193c036afb82571a209bf35a82178e30fb65
NoxGamingQC <noxracinggaming@gmail.com>
2019-07-30 23:25:10 -0400

    Fix website title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @if(env('APP_ENV', 'developement'))
-    <title>Dev - {{env('APP_NAME')}} - @yield('title')</title>
+    <title>{{env('APP_ENV') == 'developement' ? 'Dev - ' : ''}}{{env('APP_NAME')}} - @yield('title')</title>
     @else
     <title>{{env('APP_NAME')}} - @yield('title')</title>
     @endif
