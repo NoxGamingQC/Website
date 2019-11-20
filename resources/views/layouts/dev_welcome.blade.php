0242f70ab25b1557aed65bd9e3447f2329812204
NoxGamingQC <noxracinggaming@gmail.com>
2019-11-20 16:13:10 -0500

    Adding dev log in

---
+++ resources/views/layouts/dev_welcome.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @if(env('APP_ENV', 'developement'))
+    <title>{{env('APP_ENV') == 'developement' ? 'Dev - ' : ''}}{{env('APP_NAME')}} - @yield('title')</title>
+    @else
+    <title>{{env('APP_NAME')}} - @yield('title')</title>
+    @endif
+    <link rel="icon" href="/img/Avatar.png" type="image/png">
+    <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
+</head>
+
+<body>
+    <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
+    @yield('content')
+    @include('layouts.footer')
+    <script src="{{mix('js/app.js')}}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/i18n/defaults-*.min.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,80 +10,92 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return redirect(app()->getLocale() . '/home');
-});
 
-Route::get('/home', function () {
-    return redirect(app()->getLocale() . '/home');
-});
-
-Route::get('/noxbot/data/json/activities', 'NoxBOT\BotActivitiesController@getActivities');
-Route::get('/noxbot/data/json/modules', 'NoxBOT\BotModulesController@getModules');
-Route::get('/noxbot/data/json/subs_modules', 'NoxBOT\BotModulesController@getSubsModules');
-Route::get('/noxbot/data/json/roles_reactions', 'NoxBOT\BotRolesReactionsController@getRolesReactions');
-Route::get('/noxbot/data/json/twitch_lives', 'NoxBOT\BotTwitchLivesController@getTwitchLives');
+Auth::routes();
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+if (env('APP_ENV') !== 'production') {
+    Route::get('/welcome_dev', function () {
+        return view('welcome_dev');
+    });
+}
 
 Route::group(
     [
-    'prefix' => '{locale}',
-    'where' => ['locale' => '[a-zA-Z]{2}'],
-    'middleware' => 'setlocale'],
+    'middleware' => 'Development'],
     function () {
         Route::get('/', function () {
-            return redirect('home');
+            return redirect(app()->getLocale() . '/home');
         });
 
         Route::get('/home', function () {
-            return view('welcome');
+            return redirect(app()->getLocale() . '/home');
         });
 
-        Route::get('/projects', function () {
-            return view('projects');
-        });
+        Route::get('/noxbot/data/json/activities', 'NoxBOT\BotActivitiesController@getActivities');
+        Route::get('/noxbot/data/json/modules', 'NoxBOT\BotModulesController@getModules');
+        Route::get('/noxbot/data/json/subs_modules', 'NoxBOT\BotModulesController@getSubsModules');
+        Route::get('/noxbot/data/json/roles_reactions', 'NoxBOT\BotRolesReactionsController@getRolesReactions');
+        Route::get('/noxbot/data/json/twitch_lives', 'NoxBOT\BotTwitchLivesController@getTwitchLives');
 
-        Route::get('/stream', function () {
-            return view('stream');
-        });
+        Route::group(
+            [
+            'prefix' => '{locale}',
+            'where' => ['locale' => '[a-zA-Z]{2}'],
+            'middleware' => 'setlocale'],
+            function () {
+                Route::get('/', function () {
+                    return redirect('home');
+                });
 
-        Route::get('/stream/commands', function () {
-            return view('commands');
-        });
+                Route::get('/home', function () {
+                    return view('welcome');
+                });
+
+                Route::get('/projects', function () {
+                    return view('projects');
+                });
+
+                Route::get('/stream', function () {
+                    return view('stream');
+                });
+
+                Route::get('/stream/commands', function () {
+                    return view('commands');
+                });
 
 
-        Route::get('/noxbot', function () {
-            return view('noxbot');
-        });
+                Route::get('/noxbot', function () {
+                    return view('noxbot');
+                });
 
-        Route::get('/noxbot/dashboard', function () {
-            return view('noxbot_dashboard');
-        });
-
-
-        Route::get('/games', function () {
-            return view('games');
-        });
-
-        Route::get('/noxbot/dashboard', 'NoxBotDashboardController@index');
+                Route::get('/noxbot/dashboard', function () {
+                    return view('noxbot_dashboard');
+                });
 
 
-        Route::get('/profile/show/{id}', 'UserProfileController@index');
+                Route::get('/games', function () {
+                    return view('games');
+                });
 
-        Route::get('/profile/edit', 'UserProfileController@edit');
+                Route::get('/noxbot/dashboard', 'NoxBotDashboardController@index');
 
-        Route::get('/contact', function () {
-            return view('contact');
-        });
 
-        Route::get('/noxbot/dashboard/{serverID}', 'NoxBotDashboardController@serverDashboard');
+                Route::get('/profile/show/{id}', 'UserProfileController@index');
 
-        Auth::routes();
-        Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+                Route::get('/profile/edit', 'UserProfileController@edit');
 
-        Route::post('/noxbot/data/user/store', 'NoxBotDashboardController@linkDiscord');
+                Route::get('/contact', function () {
+                    return view('contact');
+                });
 
-        Route::get('/management/modules', 'ManagementController@getModules');
-        Route::get('/management/users', 'ManagementController@getUsers');
-        Route::get('/management/activities', 'ManagementController@getBotActivities');
+                Route::get('/noxbot/dashboard/{serverID}', 'NoxBotDashboardController@serverDashboard');
+
+                Route::post('/noxbot/data/user/store', 'NoxBotDashboardController@linkDiscord');
+
+                Route::get('/management/modules', 'ManagementController@getModules');
+                Route::get('/management/users', 'ManagementController@getUsers');
+                Route::get('/management/activities', 'ManagementController@getBotActivities');
+            }
+        );
     }
 );
