07474febd78707d92038afec82296de2d65ee7fb
NoxGamingQC <noxracinggaming@gmail.com>
2020-05-04 18:56:07 -0400

    fix cover

--- resources/views/games.blade.php
+++ resources/views/games.blade.php
@@ -183,7 +183,7 @@
                             @foreach($pc as $key => $console)
                             <div class="col-md-3">
                                 <div class="panel panel-primary">
-                                    <div class="panel-body" style="background-image: url('https://static-cdn.jtvnw.net/ttv-boxart/{{$console->Game}}-285x380.jpg'); background-size: cover; !important">
+                                    <div class="panel-body" style="background-image: url('https://static-cdn.jtvnw.net/ttv-boxart/{{$console->Game}}-285x380.jpg') !important; background-size: cover; !important">
                                         <h4 class="stroke"><b>{{$console->Game}}</b></h4>
                                         <hr />
                                         <p class="stroke">{{trans('generic.format')}}: {{$console->format}}</p>
