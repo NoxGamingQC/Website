51ae4ce43922b1f0a11493965e5043acba69d21c
Jimmy Béland-Bédard <noxracinggaming@gmail.com>
2020-07-08 11:15:50 -0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="NoxGamingQC's Website">
+    <meta name="author" content="NoxGamingQC">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="{{mix('js/app.js')}}"></script>
