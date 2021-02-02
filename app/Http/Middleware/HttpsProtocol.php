4a37df75e0af8312e8cce0bb8cf00f4d8727f121
NoxGamingQC <noxgamingqc@gmail.com>
2021-02-01 23:07:08 -0500

    Adding secure connection while in production

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -4,13 +4,15 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\App;
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\Redirect;
 
 class HttpsProtocol {
 
     public function handle($request, Closure $next)
     {
         if (!$request->secure() && App::environment() === 'production') {
-            return redirect()->secure($request->getRequestUri);
+            return redirect()->secure($request->getRequestUri(), 301, ['X-Forwarded-Proto'=> [true]]);
         }
 
         return $next($request); 
