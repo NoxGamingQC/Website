a571ccc5643469f39f00a173c44fe9854422d772
NoxGamingQC <noxgamingqc@gmail.com>
2021-02-02 00:50:37 -0500

    Changing dev page name

--- resources/views/layouts/overlay.blade.php
+++ resources/views/layouts/overlay.blade.php
@@ -1,27 +1,26 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="{{mix('js/app.js')}}"></script>
-    @if(env('APP_ENV', 'developement'))
-    <title>{{env('APP_ENV') == 'developement' ? 'Dev - ' : ''}}{{env('APP_NAME')}} - @yield('title')</title>
-    @else
-    <title>{{env('APP_NAME')}} - @yield('title')</title>
-    @endif
-    <link rel="icon" href="/img/Avatar.png" type="image/png">
-    <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
-</head>
-
-<body class="remove-background">
-    @yield('content')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/i18n/defaults-*.min.js"></script>
-</body>
-
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script src="{{mix('js/app.js')}}"></script>
+        @if(env('APP_ENV', 'developement'))
+        <title>{{env('APP_ENV') == 'developement' ? 'Dev - ' : ''}}{{env('APP_NAME')}} - @yield('title')</title>
+        @else
+        <title>{{env('APP_NAME')}} - @yield('title')</title>
+        @endif
+        <link rel="icon" href="/img/Avatar.png" type="image/png">
+        <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{mix('css/bootstrap.css')}}" rel="stylesheet" type="text/css">
+            <link href="{{mix('css/defaultSkin.css')}}" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
+    </head>
+    <body class="remove-background">
+        @yield('content')
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/i18n/defaults-*.min.js"></script>
+    </body>
 </html>
