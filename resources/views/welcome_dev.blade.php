eaa9448ef064477de5b244caa90df77d0014fd3a
NoxGamingQC <noxgamingqc@gmail.com>
2021-02-02 00:28:42 -0500

    Remodling the dev page

--- resources/views/welcome_dev.blade.php
+++ resources/views/welcome_dev.blade.php
@@ -1,26 +1,34 @@
 @extends('layouts.dev_welcome')
 @section('title', 'Stream')
 @section('content')
-<h1>DEVELOPMENT MODE</h1>
+<h1>On maintenance. / En maintenance.</h1>
 <hr />
 <div class="container">
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-primary">
-                <div class="panel-heading">DEVELOPMENT MODE ENGAGED.</div>
+                <div class="panel-heading">On maintenance / En maintenance</div>
 
                 <div class="panel-body">
-                    <h3>What to do</h3>
-                    <hr />
-                    <p>To access this website on this current state you must be logged in at all time and have the necessary right. If it's not your case don't worry the website should be back shortly. If you're one of the developement team member, want to help and don't have your credential make sure to ask NoxGamingQC in DM on Discord.</p>
-                    <hr />
+                    <h3>Sorry / Désolé</h3>
+
+                    <p>Something went wrong and we decided to put the website on mainteance for a short time. Thanks for your patience. We should be back soon.</p>
+                    <br />
+                    <p>Nous avons encontré une erreur et nous avons décidé de mettre le site web en maintence. Merci de votre patience. Nous seront de retour bientôt.</p>
+                    <br />
                     @guest
-                        <a class="btn btn-primary" href="{{ route('login', app()->getLocale()) }}">Login</a>
-                        <a class="btn btn-primary" href="{{ route('register', app()->getLocale()) }}">Register</a>
+                        <em>P.S. Only members with developpers rights have access the website at the moment.</em>
+                        <br />
+                        <em>P.S. Seul les membres ayant les droits développeur ont accéder au site web pour le moment.</em>
+                        <br />
+                        <br />
+                        <a class="btn btn-primary" href="{{ route('login', app()->getLocale()) }}">Login / Se connecter</a>
+                        <a class="btn btn-primary" href="{{ route('register', app()->getLocale()) }}">Register / S'inscrire</a>
+
                     @endguest
                     @auth
-                        <a class="btn btn-primary disabled" href="#" disabled>Login</a>
-                        <a class="btn btn-primary disabled" href="#" disabled>Register</a>
+                        <a class="btn btn-primary disabled" href="#" disabled>Login / Se connecter (dev only)</a>
+                        <a class="btn btn-primary disabled" href="#" disabled>Register / S'inscrire</a>
                         <hr />
                         P.S. Buttons are disabled, because we are detecting that your are already logged in. ¯\_(ツ)_/¯ <a href="/{{app()->getLocale()}}/home">(Travel to home page)</a>
                     @endauth
