49b34fcfe1d49d3f3a047b746a2d4e9ade968603
NoxGamingQC <noxgamingqc@gmail.com>
2021-02-01 22:00:33 -0500

    Adding secure connection while in production

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -10,7 +10,6 @@ class HttpsProtocol {
     public function handle($request, Closure $next)
     {
             if (!$request->secure() && App::environment() === 'production') {
-                \URL::forceScheme('https');
                 $request->header->add('X-Forwarded-Proto');
                 return redirect()->secure($request->getRequestUri());
             }
