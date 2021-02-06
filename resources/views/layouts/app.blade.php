4187d089a74753ad70acf821c666779534d95ace
NoxGamingQC <noxgamingqc@gmail.com>
2021-02-05 22:31:28 -0500

    Fixing csrf token error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,18 +30,15 @@
             <link href="{{mix('css/defaultSkin.css')}}" rel="stylesheet" type="text/css">
         @endif
     @endauth
-    <link rel="stylesheet" href="/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
 </head>
 
 <body>
-    <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
     @include('layouts.navbar')
     @include('alert.alert')
     @yield('content')
     @include('layouts.footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/i18n/defaults-*.min.js"></script>
 </body>
 
 </html>
--- resources/views/modal/register.blade.php
+++ resources/views/modal/register.blade.php
@@ -29,7 +29,7 @@
                             <label for="email" class="col-md-4 control-label">{{trans('generic.email_address')}}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="RegisterEmail" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
                                 <span class="help-block">
@@ -43,7 +43,7 @@
                             <label for="password" class="col-md-4 control-label">{{trans('generic.password')}}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="registerPassword" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
                                 <span class="help-block">
