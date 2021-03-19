ad7ec9adf32da5586bc02d447038b0854e8300a2
NoxGamingQC <noxgamingqc@gmail.com>
2021-03-18 23:28:25 -0400

    Remove world from database

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Khsing\World\WorldServiceProvider::class,
 
 
         /*
