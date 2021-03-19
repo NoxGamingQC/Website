4de9c4ca55b244412ddd6bb55cfb3dd8784676fb
NoxGamingQC <noxgamingqc@gmail.com>
2021-03-19 14:04:59 -0400

    Created error 419 page

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('title', 'METHOD NOT ALLOWED')
+@section('content')
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-primary-red">
+            <div class="panel-body">
+                <h3>ERROR 419 - The page has expired due to inactivity.</h3>
+                <hr />
+                <p>What took you so long?</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+@stop
