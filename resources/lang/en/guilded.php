f8202e2726b0b924d3432d7627501c8573700309
NoxGamingQC <noxgamingqc@gmail.com>
2021-04-26 19:43:25 -0400

    Switching from discord to guilded

--- resources/lang/en/guilded.php
+++ resources/lang/en/guilded.php
@@ -2,9 +2,9 @@
 
 return [
     'guilded_subscription' => "Guilded subscription",
-    'copper' => "Copper",
-    'silver' => "Silver",
-    'gold' => "Gold",
+    'copper' => "Copper ─ 5\$CAD",
+    'silver' => "Silver ─ 10\$CAD",
+    'gold' => "Gold ─ 20\$CAD",
     'copper_description' => "<li>Shiny copper profile flair</li><li>Exclusive <copper>@Copper</copper> role</li><li>Support Nox and his stream/videos</li><li>More comming soon...</li>",
     'silver_description' => "<li>Shiny silver profile flair</li><li>Exclusive <silver>@Silver</silver> role</li><li>Support Nox and his stream/videos</li><li>More comming soon...</li>",
     'gold_description' => "<li>Shiny gold profile flair</li><li>Exclusive <gold>@Gold</gold> role</li><li>Support Nox and his stream/videos</li><li>More comming soon...</li>"
--- resources/lang/fr/guilded.php
+++ resources/lang/fr/guilded.php
@@ -2,9 +2,9 @@
 
 return [
     'guilded_subscription' => "Abonnement Guilded",
-    'copper' => "Cuivre",
-    'silver' => "Argent",
-    'gold' => "Or",
+    'copper' => "Cuivre ─ 5\$CAD",
+    'silver' => "Argent ─ 10\$CAD",
+    'gold' => "Or ─ 20\$CAD",
     'copper_description' => "<li>L'étiquette de profile cuivre</li><li>Le role exclusif <copper>@Copper</copper></li><li>Supportez Nox et ses diffusion/vidéos</li><li>Plus à venir...</li>",
     'silver_description' => "<li>L'étiquette de profile argent</li><li>Le role exclusif <silver>@Silver</silver></li><li>Supportez Nox et ses diffusion/vidéos</li><li>Plus à venir...</li>",
     'gold_description' => "<li>L'étiquette de profile or</li><li>Le role exclusif <gold>@Gold</gold></li><li>Supportez Nox et ses diffusion/vidéos</li><li>Plus à venir...</li>"
