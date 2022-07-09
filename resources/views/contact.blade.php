1d1010ea573a8c028f3d6e530d66e04a4a7a1f4a
NoxGamingQC <noxgamingqc@gmail.com>
2022-07-09 19:15:55 -0400

    Fix contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,73 +16,56 @@
                     <a class="btn btn-twitch btn-lg" href="https://www.twitch.tv/noxgamingqc">
                         <i class="fa fa-twitch fa-5x" aria-hidden="true"></i>
                     </a>
-                    <p>NoxGamingQC</p>
                 </div>
                 <div class="col-md-2" id="youtube">
                     <a class="btn btn-youtube btn-lg" href="https://www.youtube.com/channel/UCytKDUapog2tnJD4XenehiQ">
                         <i class="fa fa-youtube-play fa-5x" aria-hidden="true"></i>
                     </a>
-                    <p>NoxGamingQC</p>
                 </div>
                 <div class="col-md-2" id="twitter">
                     <a class="btn btn-twitter btn-lg" href="https://www.twitter.com/@noxgamingqc">
                         <i class="fa fa-twitter fa-5x" aria-hidden="true"></i>
                     </a>
-                    <p>@NoxGamingQC</p>
                 </div>
                 <div class="col-md-2" id="instagram">
                     <a class="btn btn-instagram btn-lg" href="https://www.instagram.com/noxgamingqc">
                         <i class="fa fa-instagram fa-5x" aria-hidden="true"></i>
                     </a>
-                    <p>@NoxGamingQC</p>
                 </div>
                 <div class="col-md-2" id="steam">
                     <a class="btn btn-steam btn-lg" href="http://steamcommunity.com/id/NoxGamingQC">
                         <i class="fa fa-steam-square fa-5x" aria-hidden="true"></i>
                     </a>
-                    <p>NoxGamingQC</p>
-                </div>
-                <div class="col-md-2" id="googlePlus">
-                    <a class="btn btn-google-plus btn-lg" href="https://plus.google.com/u/0/114349968576126625828">
-                        <i class="fa fa-google-plus-circle fa-5x" aria-hidden="true"></i>
-                    </a>
-                    <p>NoxGamingQC</p>
                 </div>
                 <div class="col-md-2" id="github">
                     <a class="btn btn-github btn-lg" href="https://github.com/noxgamingqc">
                         <i class="fa fa-github fa-5x" aria-hidden="true"></i>
                     </a>
-                    <p>NoxGamingQC</p>
                 </div>
                 <div class="col-md-2" id="snapchat">
                     <a class="btn btn-snapchat btn-lg" href="https://www.snapchat.com/add/NoxGamingQC">
                         <i class="fa fa-snapchat-ghost fa-5x" aria-hidden="true"></i>
                     </a>
-                    <p>NoxGamingQC</p>
                 </div>
                 <div class="col-md-2" id="spotify">
                     <a class="btn btn-spotify btn-lg" href="https://open.spotify.com/user/howlnox22607">
                         <i class="fa fa-spotify fa-5x" aria-hidden="true"></i>
                     </a>
-                    <p>HowlNox22607</p>
                 </div>
                 <div class="col-md-2" id="reddit">
                     <a class="btn btn-reddit btn-lg" href="https://www.reddit.com/user/NoxRacing">
                         <i class="fa fa-reddit fa-5x" aria-hidden="true"></i>
                     </a>
-                    <p>NoxRacing</p>
                 </div>
                 <div class="col-md-2" id="pinterest">
                     <a class="btn btn-pinterest btn-lg" href="https://www.pinterest.ca/noxgamingqc/">
                         <i class="fa fa-pinterest fa-5x" aria-hidden="true"></i>
                     </a>
-                    <p>NoxGamingQC</p>
                 </div>
                 <div class="col-md-2" id="skype">
                     <a class="btn btn-skype btn-lg" href="skype:howlnox22607?userinfo">
                         <i class="fa fa-skype fa-5x" aria-hidden="true"></i>
                     </a>
-                    <p>HowlNox22607</p>
                 </div>
             </div>
         </div>
