5346892830a95a9d69148f14bdaf9217996ee95f
NoxGamingQC <noxgamingqc@gmail.com>
2022-07-10 10:24:38 -0400

    Fix menues

--- public/js/app.js
+++ public/js/app.js
@@ -2096,7 +2096,7 @@ window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
  */
 
 try {
-  window.$ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+  window.$ = window.jQuery = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
   __webpack_require__(/*! bootstrap-sass */ "./node_modules/bootstrap-sass/assets/javascripts/bootstrap.js");
 } catch (e) {}
