45c341652e798fa2ee2bdb2213fdbd5301c5bc7d
NoxGamingQC <noxgamingqc@gmail.com>
2022-07-10 10:24:38 -0400

    Fix menues

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -8,7 +8,7 @@ window._ = require('lodash');
  */
 
 try {
-    window.$ = require('jquery');
+    window.$ = window.jQuery = require('jquery');
     
     require('bootstrap-sass');
 } catch (e) {}
