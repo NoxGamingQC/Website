99a18ad9445ac287b2ec6659fb116206b171a6d0
NoxGamingQC <noxgamingqc@gmail.com>
2022-07-11 22:56:44 -0400

    Fix services

--- resources/lang/fr/services.php
+++ resources/lang/fr/services.php
@@ -8,10 +8,11 @@ return [
     'diagnostics' => "Diagnostique (Windows ou Linux seulement)",
     'diagnostics_info' => "Sera remboursé si le problème n'a pas été trouvé",
     'computer_other' => "Tout les autres problème informatique, le coût peut être sujet à changement dépendant des traveaux a effectuer. Le travail peut être refusé si non décris correctement.",
-    'traveling_fee_above' => "Des frais de déplacement de 0.50\$CAD/km s'applique sur tout les items listé ci-haut. Ne peut être remboursé.",
+    'traveling_fee_above' => "Des frais de déplacement de 0.75\$CAD/km s'applique sur tout les items listé ci-haut. Ne peut être remboursé.",
     'taxes' => "Taxes peut s'appliquer. (TPS/TVQ)",
     'delivery' => "Livraison",
     'lift' => "Covoiturage",
     'base_fees' => "Frais de base",
-    'licence_not_included' => "Licence non incluse"
+    'licence_not_included' => "Licence non incluse",
+    'price_may_change' => "Les prix sont sujet à changement sans préavis."
 ];
