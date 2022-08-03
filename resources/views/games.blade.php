a5d6c553c19db001a38208beaf2c11cb45d530ff
NoxGamingQC <noxgamingqc@gmail.com>
2022-08-03 14:22:40 -0400

    Fix games

--- resources/views/games.blade.php
+++ resources/views/games.blade.php
@@ -24,42 +24,38 @@
         </div>
     </div>
     <div class="col-md-12">
-        <hr />
-        <div class="panel panel-primary">
-            <div class="panel-body">
-            @foreach($consoles as $key=>$console)
-                <h3>{{$console->Console}} ({{count($gamesList[$console->id])}})</h3>
-                    <hr />
-                    <div class="row">
-                        <div class="col-md-12">
-                            @php ($currentCount = 0)
-                            @foreach($gamesList[$console->id] as $key => $game)
-                            <div class="col-md-3">
-                                <div class="panel panel-primary text-center">
-                                    <div class="panel-body" style="background-image: url('https://static-cdn.jtvnw.net/ttv-boxart/{{$game->Game}}-285x380.jpg') !important; background-size: cover !important; height: 380px !important;">
-                                        <h2 class="game-title"><b>{{preg_replace('/\\\\/', '', $game->Game)}}</b></h2>
-                                        <input id="gameName-{{$game->id}}" type="hidden" value="{{$game->Game}}">
-                                        <input id="gameConsole-{{$game->id}}" type="hidden" value="{{$game->Console}}">
-                                        <input id="gameDate-{{$game->id}}" type="hidden" value="{{$game->Date}}">
-                                        <input id="gameCoverURL-{{$game->id}}" type="hidden" value="{{$game->CoverURL}}">
-                                        <input id="gameFormat-{{$game->id}}" type="hidden" value="{{$game->format}}">
-                                        <button id="{{$game->id}}" class="edit-game-button btn btn-info" type="button" data-toggle="modal" data-target="#editGameModal">{{trans('generic.see_more')}}</button>
-                                    </div>
-                                </div>
+    <hr />
+    @foreach($consoles as $key=>$console)
+        <h3>{{$console->Console}} ({{count($gamesList[$console->id])}})</h3>
+            <br />
+            <div class="row">
+                <div class="col-md-12">
+                    @php ($currentCount = 0)
+                    @foreach($gamesList[$console->id] as $key => $game)
+                    <div class="col-md-3">
+                        <div class="panel panel-primary text-center">
+                            <div class="panel-body" style="background-image: url('https://static-cdn.jtvnw.net/ttv-boxart/{{$game->Game}}-285x380.jpg') !important; background-size: cover !important; height: 380px !important;">
+                                <h2 class="game-title"><b>{{preg_replace('/\\\\/', '', $game->Game)}}</b></h2>
+                                <input id="gameName-{{$game->id}}" type="hidden" value="{{$game->Game}}">
+                                <input id="gameConsole-{{$game->id}}" type="hidden" value="{{$game->Console}}">
+                                <input id="gameDate-{{$game->id}}" type="hidden" value="{{$game->Date}}">
+                                <input id="gameCoverURL-{{$game->id}}" type="hidden" value="{{$game->CoverURL}}">
+                                <input id="gameFormat-{{$game->id}}" type="hidden" value="{{$game->format}}">
+                                <button id="{{$game->id}}" class="edit-game-button btn btn-info" type="button" data-toggle="modal" data-target="#editGameModal">{{trans('generic.see_more')}}</button>
                             </div>
-                            @if($currentCount == 3)
-                                </div>
-                                <div class="col-md-12">
-                                @php ($currentCount = 0)
-                            @else
-                                @php ($currentCount += 1)
-                            @endif
-                            @endforeach
                         </div>
                     </div>
+                    @if($currentCount == 3)
+                        </div>
+                        <div class="col-md-12">
+                        @php ($currentCount = 0)
+                    @else
+                        @php ($currentCount += 1)
+                    @endif
                     @endforeach
                 </div>
             </div>
+            @endforeach
         </div>
     </div>
 </div>
