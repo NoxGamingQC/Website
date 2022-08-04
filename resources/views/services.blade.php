86ab2c92876b31c2e3547cf1bcf3b75f50c9328d
NoxGamingQC <noxgamingqc@gmail.com>
2022-08-04 16:25:00 -0400

    Adding square strip

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -77,6 +77,13 @@
                         </div>
                     </div>
                 </div>
+                <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <br />
+                    @include('layouts.square_strip')
+                    <br />
+                </div>
+            </div>
             </div>
         </div>
     </div>
