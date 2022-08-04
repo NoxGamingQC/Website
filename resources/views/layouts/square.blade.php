865b90cc3f13140cb1fdffe8785b5f0146b087b8
NoxGamingQC <noxgamingqc@gmail.com>
2022-08-04 16:25:00 -0400

    Adding square strip

---
+++ resources/views/layouts/square.blade.php
@@ -0,0 +1,20 @@
+<div class="text-center" style="max-width: 550px;">
+    <div class="text-center" style="background-color:#fff; padding:75px; padding-left: 100px; max-width: 550px; border-radius: 50px;">
+        <div style="margin-bottom:3%">
+            <img src="/img/payments/Square.png" width="400" style="margin-left:-50px;"/>
+        </div>
+        <div>
+            <img src="/img/payments/Visa.png" width="100" style="margin-right:5%"/>
+            <img src="/img/payments/Mastercard.png" width="100" style="margin-right:5%"/>
+            <img src="/img/payments/Amex.png" width="100" style="margin-right:5%"/>
+        </div>
+        <div>
+            <img src="/img/payments/Interac.png" width="60" style="margin-left: 10px;margin-right:5%"/>
+            <img src="/img/payments/Apple.png" width="100" style="margin-right:5%"/>
+            <img src="/img/payments/Google.png" width="100" style="margin-right:5%"/>
+        </div>
+    </div>
+    <span class="text-copyright"><i>&copy Square, Inc. Square and the Square logo are trademarks of Square, Inc.</i></span>
+    <br />
+    <span class="text-copyright"><i>Other trademarks and brands are the property of their respective owners.</i></span>
+</div>
---
+++ resources/views/layouts/square_strip.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <div class="text-center" style="background-color:#fff; padding:25px; max-width: 700px; border-radius: 25px;">
+        <img src="/img/payments/Square.png" width="200" style="margin-right:2%"/>
+        <img src="/img/payments/Visa.png" width="50" style="margin-right:2%"/>
+        <img src="/img/payments/Mastercard.png" width="50" style="margin-right:2%"/>
+        <img src="/img/payments/Amex.png" width="50" style="margin-right:2%"/>
+        <img src="/img/payments/Interac.png" width="50" style="margin-left: 10px;margin-right:2%"/>
+        <img src="/img/payments/Apple.png" width="50" style="margin-right:2%"/>
+        <img src="/img/payments/Google.png" width="50" style="margin-right:2%"/>
+    </div>
+    <span class="text-copyright"><i>&copy Square, Inc. Square and the Square logo are trademarks of Square, Inc. Other trademarks and brands are the property of their respective owners.</i></span>
+</div>
