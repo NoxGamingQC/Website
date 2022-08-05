f127251720f992073795ba8fa18620dc992a9dda
NoxGamingQC <noxgamingqc@gmail.com>
2022-08-05 08:49:28 -0400

    game description

--- resources/lang/en/game.php
+++ resources/lang/en/game.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'page_description' => "A list of game that Nox's own. P.S. A tag will appear on games that Nox's have played and have uploaded on YouTube.",
     'add_console' => "Add a console",
     'add_game' => "Add a game",
     'game' => "Game",
