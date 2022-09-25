d9b96ac9d733e2fadc8c91d88d1a99117d5fc457
NoxGamingQC <noxgamingqc@gmail.com>
2022-09-25 14:57:27 -0400

    Fix show profile

--- resources/views/profile_show.blade.php
+++ resources/views/profile_show.blade.php
@@ -5,8 +5,26 @@
     <div class="col-md-12">
         <div class="panel panel-block-primary">
             <div class="panel-body" style="padding: 5%">
-                <div class="text-center">
-                    <img class="img-circle" src="{{$avatarURL}}" alt="{{$discordName}}" width="250px" style="padding: 7px 14px" />
+                @if ($country)
+                    @if($isPremium)
+                    <div class="col-md-6 text-left">
+                    @else
+                    <div class="col-md-12 text-left">
+                    @endif
+                        <img class="img-circle" src="https://cdn.countryflags.com/thumbs/{{str_replace(' ', '-', strtolower($country))}}/flag-square-500.png" alt="{{$country}}" title="{{$country}}" width="40px" />
+                    </div>
+                @endif
+                @if($isPremium)
+                    @if($country)
+                    <div class="col-md-6 text-right">
+                    @else
+                    <div class="col-md-12 text-right">
+                    @endif
+                        <img src="/img/badges/premium.png" alt="{{trans('profile.premium')}}" title="{{trans('profile.premium')}}" width="40px" />
+                    </div>
+                @endif
+                <div class="col-md-12 text-center">
+                    <img class="img-circle" src="{{$avatarURL}}" alt="{{$username}}" title="{{$username}}" width="250px" style="padding: 7px 14px" />
                     <h1>
                         {{$username}}
                         <br />
@@ -14,7 +32,7 @@
                     </h1>
                     <h3>{{ trans('profile.' . $grade) }}</h3>
                     @foreach ($badges as $badge)
-                        <img src="/img/Badges/{{$badge}}.png" alt="{{$badge}}" width="75px" style="padding: 7px 14px" />
+                        <img src="/img/Badges/{{$badge}}.png" alt="{{$badge}}" title="{{$badge}}" width="75px" style="padding: 7px 14px" />
                     @endforeach
                 </div>
             </div>
@@ -24,12 +42,9 @@
         <h3>{{ trans('profile.user_acknowledgement') }}</h3>
         <br />
         <ul>
-            <li><b>{{ trans('profile.account_level') }}:</b> {{ trans('profile.' . $grade) }}</li>
-            <li><b>{{ trans('profile.premium_user') }}:</b> {{$isPremium ? trans('profile.premium') : trans('profile.not_premium')}}</li>
             @if($gender)<li><b>{{ trans('profile.gender') }}:</b> {{$gender}}</li>@endif
             @if($birthdate)<li><b>{{ trans('profile.birthdate') }}:</b> {{$birthdate}}</li>@endif
             @if($age)<li><b>{{ trans('profile.age') }}:</b> {{$age}}</li>@endif
-            <li><b>{{ trans('profile.country') }}:</b> {{$country}}</li>
         </ul>
     </div>
 </div>
