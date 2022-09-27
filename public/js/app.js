0b5a29d98b6919e4b3388b6e2de4f8de29c4c3c8
NoxGamingQC <noxgamingqc@gmail.com>
2022-09-27 18:22:54 -0400

    Fix status

--- public/js/app.js
+++ public/js/app.js
@@ -2182,7 +2182,7 @@ window.fbAsyncInit = function () {
 
 var idleTime = 0;
 $(document).ready(function () {
-  var idleInterval = setInterval(timerIncrement, 15000);
+  var idleInterval = setInterval(timerIncrement, 1500);
   $(this).mousemove(function (e) {
     checkIfOnline();
     idleTime = 0;
