543e086d99db54b3db4c2835f8593a3021e39f80
NoxGamingQC <noxgamingqc@gmail.com>
2022-09-27 10:36:15 -0400

    Add some fields in managements

--- resources/views/management/users.blade.php
+++ resources/views/management/users.blade.php
@@ -18,20 +18,68 @@
                                 @else
                                     <img class="img-circle" src="/img/no-avatar.jpg" alt="{{$user['username']}}" title="{{$user['username']}}" width="100px" style="padding-bottom: 5px" />
                                 @endif
-                                <h4><b>{{$user['username']}}{{ $user['isBOT'] ? ' [BOT]' : ''}}</b></h4>
+                                <h4>
+                                    <b>
+                                        @if($user['isPremium'])
+                                            <i class="fa fa-star text-warning" aria-hidden="true"></i>
+                                         @endif   
+                                        {{$user['username']}}{{ $user['isBOT'] ? ' [BOT]' : ''}}
+                                        @if($user['isVerified'])
+                                        <i class="fa fa-check-circle-o text-success" aria-hidden="true"></i>
+                                        @endif
+                                    </b>
+                                </h4>
                                 <br />
-                                <select class="selectpicker" id="gender" title="Gender">
-                                    <option value="0" {{$user['grade'] == 'member' ? 'selected' : ''}}>Member</option>
-                                    <option value="1" {{$user['grade'] == 'developper' ? 'selected' : ''}}>Developper</option>
-                                    <option value="2" {{$user['grade'] == 'moderator' ? 'selected' : ''}}>Moderator</option>
-                                    <option value="3" {{$user['grade'] == 'administrator' ? 'selected' : ''}}>Administrator</option>
-                                </select>
+                                <div class="row text-left">
+                                    <div class="col-md-12">
+                                        <select class="selectpicker" id="grade-{{$user['id']}}" title="Grade">
+                                            <option value="0" {{$user['grade'] == 'member' ? 'selected' : ''}}>Member</option>
+                                            <option value="1" {{$user['grade'] == 'developper' ? 'selected' : ''}}>Developper</option>
+                                            <option value="2" {{$user['grade'] == 'moderator' ? 'selected' : ''}}>Moderator</option>
+                                            <option value="3" {{$user['grade'] == 'administrator' ? 'selected' : ''}}>Administrator</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <label for="country">Country: </label>
+                                    </div>
+                                    <div class="col-md-9">
+                                            <input type="text" class="form-control" id="country-{{$user['id']}}" placeholder="Country" value="{{$user['country']}}" />
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label>Bot: </label><input type="checkbox" id="isBOT-{{$user['id']}}" value="{{$user['isBOT'] }}" {{ $user['isBOT'] ? 'checked' : '' }} />
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label>Email subs: </label> <input type="checkbox" id="isEmailSubs-{{$user['id']}}" value="{{$user['isEmailSubscriber'] }}" {{ $user['isEmailSubscriber'] ? 'checked' : '' }} />
+                                    </div>
+                                    <div class="col-md-4">
+                                        <label for="country">Discord ID: </label>
+                                    </div>
+                                    <div class="col-md-8">
+                                            <input type="text" class="form-control" id="discordID-{{$user['id']}}" placeholder="Discord ID" value="{{$user['discordID']}}" />
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="col-md-12">
+                                            <label>Discord Name: </label>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="col-md-6">
+                                                <input type="text" class="form-control" id="discordName-{{$user['id']}}" placeholder="Discord name" value="{{$user['discordName']}}" />
+                                            </div>
+                                            <div class="col-md-1">
+                                                <p>#</p>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <input type="text" class="form-control" id="discriminator-{{$user['id']}}" placeholder="0000" value="{{$user['discriminator']}}" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                                 <hr />
                                 <div class="col-md-6 text-left">
                                     <a class="btn btn-primary" href="/{{app()->getLocale()}}/profile/show/{{$user['id']}}">View</a>
                                 </div>
                                 <div class="col-md-6 text-right">
-                                    <button class="btn btn-success disabled" type="button" disabled>Save</button>
+                                    <button class="btn btn-success disabled" id="{{$user['id']}}" type="button" disabled>Save</button>
                                 </div>
                             </div>
                         </div>
