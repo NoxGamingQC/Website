bf0a7091b2636fa6d6d9a2b5f16656f6ea194667
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-17 12:04:48 -0400

    Add controlle

---
+++ app/Http/Controllers/NoxBOT/PointSystemControlle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\NoxBOT;
+
+use Auth;
+use DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PointSystemController extends Controller
+{
+    public function addPoints(Request $request)
+    {
+        abort(503);
+    }
+}
