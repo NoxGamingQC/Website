21e04fb3376667a0431a5a4bae34961aea626652
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-18 18:02:33 -0400

    Add waiting for driver

--- resources/views/ngst/kiosk/fusee.blade.php
+++ resources/views/ngst/kiosk/fusee.blade.php
@@ -11,29 +11,36 @@
             <br />
             <br />
         </div>
-        <div class="col-md-12 text-center">
-            @if($kiosk[0]->AvatarURL)
-                <img class="img-circle" src="{{$kiosk[0]->AvatarURL}}" width="100" />
-            @else
-                <img class="img-circle" src="/img/no-avatar.jpg" width="100" />
-            @endif
-        </div>
-        <div class="col-md-12 text-center">
-            <h1><i>{{strtoupper($kiosk[0]->Firstname)}} {{strtoupper(substr($kiosk[0]->Lastname, 0, 1))}}.</i></h1>
-            <h3>est en route vers le restaurant.</h3>
-            <h5>{{$kiosk[0]->time}}</h5>
-        </div>
-    </div>
-    
-    <div class="row">
-        <div class="col-md-12 text-right">
-            @foreach($kiosk as $key => $value)
-                @if($key >= 1)
-                    <p><i>{{strtoupper($value->Firstname)}} {{strtoupper(substr($value->Lastname, 0, 1))}}. - {{$value->time}}</i></p>
+        @if(count($kiosk) > 0)
+            <div class="col-md-12 text-center">
+                @if($kiosk[0]->AvatarURL)
+                    <img class="img-circle" src="{{$kiosk[0]->AvatarURL}}" width="100" />
+                @else
+                    <img class="img-circle" src="/img/no-avatar.jpg" width="100" />
                 @endif
-            @endforeach
+            </div>
+            <div class="col-md-12 text-center">
+                <h1><i>{{strtoupper($kiosk[0]->Firstname)}} {{strtoupper(substr($kiosk[0]->Lastname, 0, 1))}}.</i></h1>
+                <h3>est en route vers le restaurant.</h3>
+                <h5>{{$kiosk[0]->time}}</h5>
+            </div>
         </div>
-    </div>
+        <div class="row">
+            <div class="col-md-12 text-right">
+                @foreach($kiosk as $key => $value)
+                    @if($key >= 1)
+                        <p><i>{{strtoupper($value->Firstname)}} {{strtoupper(substr($value->Lastname, 0, 1))}}. - {{$value->time}}</i></p>
+                    @endif
+                @endforeach
+            </div>
+        </div>
+    @else
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h3>En attente d'un livreur...</h3>
+            </div>
+        </div>
+    @endif
 </div>
 
 @stop
