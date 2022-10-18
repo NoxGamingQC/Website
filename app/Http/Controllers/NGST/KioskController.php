68911f930e9a59646bf7ae620907d5d8bd58003a
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-18 17:51:44 -0400

    Add names

--- app/Http/Controllers/NGST/KioskController.php
+++ app/Http/Controllers/NGST/KioskController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers\NGST;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Kiosk;
+use App\User;
+use Carbon\Carbon;
 
 class KioskController extends Controller
 {
-    public function getFusee()
+    public function index($slug)
     {
-
-        return view('ngst.kiosk.fusee');
+        $kiosk = Kiosk::where('Company', $slug)->join('users', 'users.id', '=', 'kiosk.UserID')->orderBy('kiosk.created_at', 'DESC')
+        ->take(6)->get(['kiosk.Company', 'users.Firstname', 'users.Lastname', 'users.AvatarURL', 'kiosk.created_at']);
+        Carbon::setLocale('fr');
+        foreach($kiosk as $key => $value) {
+            $value->time = Carbon::parse($value->created_at)->diffForHumans();
+        }
+        Carbon::setLocale('en');
+        return view('ngst.kiosk.' . $slug)->with([
+            'kiosk' => $kiosk
+        ]);
     }
 }
--- resources/views/ngst/kiosk/fusee.blade.php
+++ resources/views/ngst/kiosk/fusee.blade.php
@@ -12,21 +12,26 @@
             <br />
         </div>
         <div class="col-md-12 text-center">
-            <img class="img-circle" src="/img/no-avatar.jpg" width="100" />
+            @if($kiosk[0]->AvatarURL)
+                <img class="img-circle" src="{{$kiosk[0]->AvatarURL}}" width="100" />
+            @else
+                <img class="img-circle" src="/img/no-avatar.jpg" width="100" />
+            @endif
         </div>
         <div class="col-md-12 text-center">
-            <h1><i>JIMMY B.</i></h1>
+            <h1><i>{{$kiosk[0]->Firstname}} {{substr($kiosk[0]->Lastname, 0, 1)}}.</i></h1>
             <h3>est en route vers le restaurant.</h3>
-            <h5>Il y a 1 minute</h5>
+            <h5>{{$kiosk[0]->time}}</h5>
         </div>
     </div>
     
     <div class="row">
         <div class="col-md-12 text-right">
-            <p><i>JIMMY B. - Il y a 5 minutes</i></p>
-            <p><i>JIMMY B. - Il y a 10 minutes</i></p>
-            <p><i>JIMMY B. - Il y a 15 minutes</i></p>
-            <p><i>JIMMY B. - Il y a 20 minutes</i></p>
+            @foreach($kiosk as $key => $value)
+                @if($key >= 1)
+                    <p><i>{{$value->Firstname}} {{substr($value->Lastname, 0, 1)}}. - {{$value->time}}</i></p>
+                @endif
+            @endforeach
         </div>
     </div>
 </div>
