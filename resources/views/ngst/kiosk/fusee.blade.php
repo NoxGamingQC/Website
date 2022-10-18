66a685e11d9c08c3cf08f483de5c3d0236da19d8
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-18 15:30:18 -0400

    Moving text

--- resources/views/ngst/kiosk/fusee.blade.php
+++ resources/views/ngst/kiosk/fusee.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts.kiosk')
 @section('content')
 
-<h1 class="text-center">This is a test.</h1>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+            <h3 class="text-center">.... est en route vers le restaurant.</h3>
+        </div>
+    </div>
+</div>
 
 @stop
