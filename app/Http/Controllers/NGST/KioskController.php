d3ac2bbe4e59e1d61d680be90582da7c681e9bc0
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-18 14:51:54 -0400

    Submit test

---
+++ app/Http/Controllers/NGST/KioskController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\NGST;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class KioskController extends Controller
+{
+    public function getFusee()
+    {
+
+        return view('ngst.kiosk.fusee')
+    }
+}
---
+++ resources/views/layouts/kiosk.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta name="google-site-verification" content="D30gPHSCahf2lVeDo0Ndgc8vI1cQvv8d1gXIZa3B2ds" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="NoxGamingQC's Website">
+    <meta name="author" content="NoxGamingQC">
+    <meta name="viewport" content="width=800, height=480, initial-scale=1, user-scalable=0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="{{mix('css/bootstrap.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
+    <script src="{{mix('js/app.js')}}"></script>
+</head>
+
+<body>
+    @yield('content')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
+</body>
+
+</html>
