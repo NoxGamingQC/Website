2ce59406def0d6d93d387222b09e9c352db70f6c
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-27 20:34:43 -0400

    Try to fix

--- resources/views/alert/alert.blade.php
+++ resources/views/alert/alert.blade.php
@@ -1,9 +1,11 @@
 
 @if (Session::has('success'))
- <div class="alert alert-success alert-dismissible" role="alert">
+<div class="row alert alert-success alert-dismissible" role="alert">
+    <div class="col-md-12">
         <button type="button" class="close" data-dismiss="alert">
             <i class="fa fa-times"></i>
         </button>
         <strong>Success !</strong> {{ session('success') }}
     </div>
+</div>
 @endif
