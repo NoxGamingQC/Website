71a99ca42765ef4610af6f000843f9b2d73698d2
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-03 20:24:23 -0400

    Add project

--- resources/lang/en-ca/projects.php
+++ resources/lang/en-ca/projects.php
@@ -7,10 +7,6 @@ return [
     'noxbot_dashboard_unavailable' => "NoxBOT dashboard is temporarly unavailable for your server. No worry it will be there soon <3",
     'pokemon' => "Pokémon: Nox's Edition",
     'pokemon_description' => "We are currently working on a Pokémon fan game. If you want some update about the game, make sure to join our Discord server and let me know.", 
-    'apps' => "Applications",
-    'apps_description' => "Our applications",
-    'games' => "Games",
-    'games_description' => "Games broadcasted over on YouTube",
-    'websites' => "Websites",
-    'websites_description' => "Our website"
+    'website_description' => "Our website:",
+    'mrliquidations_website' => "M.R. liquidations's website:"
 ];
