8bf086a3017de702823ff0de83adb32ea79abbfe
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-03 20:29:14 -0400

    Add project description

--- resources/lang/fr-ca/projects.php
+++ resources/lang/fr-ca/projects.php
@@ -2,6 +2,7 @@
 
 return [
     'projects' => "Projets",
+    'projects_description' => "Nous travaillons présentement sur les projets listés ci-dessous.",
     'noxbot_description' => "NoxBOT est un bot multitâche pour Twitch et Discord.",
     'pokemon' => "Pokémon: Édition Nox",
     'pokemon_description' => "Nous sommes en plein développement d'un jeu Pokémon fan-game. Si vous voulez des nouvelles sur le jeu, soyez sûr de rejoindre notre serveur Discord et laissez nous le savoir.", 
