316da1d15e8a65b6548b04d90b92c5e3544e21aa
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-07 13:03:08 -0500

    Fix team page

--- resources/views/layouts/noxgamingqc/header.blade.php
+++ resources/views/layouts/noxgamingqc/header.blade.php
@@ -1,7 +1,7 @@
 <div class="text-center" id="title">
     <div class="header">
         <div class="row">
-            <h1 class="raleway-font text-highlight">@yield('title')</h1>
+            <h1 class="text-highlight">@yield('title')</h1>
             <br />
             <h3 class="raleway-font text-highlight">@yield('slogan')</h3>
             <br />
