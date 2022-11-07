cf5cf2cc343d12c847445a7cb4a710c22bda2fd7
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-06 21:04:59 -0500

    Center partners picture

--- resources/views/partners.blade.php
+++ resources/views/partners.blade.php
@@ -16,14 +16,14 @@
             <div class="row">
                 <div class="col-md-4 col-md-offset-2">
                 <div class="panel panel-primary text-center">
-                    <div class="panel-body img" style="background-image: url('/img/Partners/CorpsRoyaux.png') !important; background-size: cover !important; height: 350px !important;">
+                    <div class="panel-body img" style="background-image: url('/img/Partners/CorpsRoyaux.png') !important; background-size: cover !important; height: 350px !important;background-position:center center">
                         <h3 class="title"><b>Corps Royaux</b></h3>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="panel panel-primary text-center">
-                    <div class="panel-body img" style="background-image: url('/img/Partners/Instant-Gaming.png') !important; background-size: cover !important; height: 380px !important;">
+                    <div class="panel-body img" style="background-image: url('/img/Partners/Instant-Gaming.png') !important; background-size: cover !important; height: 380px !important;background-position:center center">
                         <h3 class="title"><b>Humble Bundle</b></h3>
                     </div>
                 </div>
@@ -38,14 +38,14 @@
             <div class="row">
             <div class="col-md-4 col-md-offset-2">
                 <div class="panel panel-primary text-center">
-                    <div class="panel-body extra-life-logo" style="height: 380px !important;">
+                    <div class="panel-body extra-life-logo" style="height: 380px !important; background-position:center center">
                         <h3 class="title"><b>Extra Life</b></h3>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="panel panel-primary text-center">
-                    <div class="panel-body img" style="background-image: url('/img/Affiliates/HumbleBundle.png') !important; background-size: cover !important; height: 380px !important;">
+                    <div class="panel-body img" style="background-image: url('/img/Affiliates/HumbleBundle.png') !important; background-size: cover !important; height: 380px !important; background-position:center center">
                         <h3 class="title"><b>Humble Bundle</b></h3>
                     </div>
                 </div>
