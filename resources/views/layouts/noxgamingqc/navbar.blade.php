5928b595e2097b74815e8f9e03f5e789d6fe7522
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-22 18:34:33 -0500

    fix mobile nav

--- resources/views/layouts/noxgamingqc/navbar.blade.php
+++ resources/views/layouts/noxgamingqc/navbar.blade.php
@@ -1,12 +1,14 @@
 <nav class="navbar navbar-default" style="position:fixed;padding-top:3vh; width:100vw">
     <div class="container-fluid">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
+            <h3 class="raleway-font" style="margin-left:5%;">NoxGamingQC
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse" aria-expanded="false" style="margin-top:-2%;margin-bottom:20px;padding:4%;border-radius:10px;">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </h3>
         </div>
         <div class="collapse navbar-collapse" id="bs-navbar-collapse">
             <ul class="nav navbar-nav navbar-center">
