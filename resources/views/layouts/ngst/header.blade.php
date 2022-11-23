55130cbfcb648f3dfe0618eec364bd5dc5ab83a6
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-22 20:07:22 -0500

    Fix width of page subtitles

--- resources/views/layouts/ngst/header.blade.php
+++ resources/views/layouts/ngst/header.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <h1 class="text-highlight">@yield('title')</h1>
             <br />
-            <h3 class="raleway-font text-highlight">@yield('slogan')</h3>
+            <h3 class="raleway-font text-highlight" style="margin-left:5%;max-width:90%">@yield('slogan')</h3>
             <br />
         </div>
     </div>
