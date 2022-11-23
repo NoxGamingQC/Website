2b9266ee5ba17062d5c1844bfe7b25783190cd7c
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-22 20:10:51 -0500

    Cleanup views

--- resources/views/layouts/ngst.blade.php
+++ resources/views/layouts/ngst.blade.php
@@ -37,10 +37,10 @@
 </head>
 
 <body>
-    @include('layouts.navbar_ngst')
+    @include('layouts.ngst.navbar')
     @include('alert.alert')
     @yield('content')
-    @include('layouts.footer_ngst')
+    @include('layouts.ngst.footer')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
