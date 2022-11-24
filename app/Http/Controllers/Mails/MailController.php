b389d67e119997325f468116eb1bda38658fc6dc
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-23 21:46:15 -0500

    Try to fix parse

--- app/Http/Controllers/Mails/MailController.php
+++ app/Http/Controllers/Mails/MailController.php
@@ -56,10 +56,12 @@ class MailController extends Controller
         $message = $mailParser->parse(urldecode($request), true);
 
         $mail->object = explode('&', explode('Subject=', $message)[1])[0];
-        $mail->sender = explode('<', explode('&', explode('From=', $message)[1])[0])[0];
+        $mail->sender_name = explode('<', explode('&', explode('From=', $message)[1])[0])[0];
+        $mail->sender = explode('>', explode('<', explode('&', explode('From=', $message)[1])[0])[1])[0];
         $mail->recipient = explode('>', explode('<', explode('&', explode('To=', $message)[1])[0])[1])[0];
         //$mail->message = $message->getHtmlContent() ? $message->getHtmlContent() : $message->getTextContent();
-        $mail->message = explode('&', explode('body-html=', $message)[1])[0];
+        $mail->html = explode('&', explode('body-html=', $message)[1])[0];
+        $mail->text = explode('&', explode('body-text=', $message)[1])[0];
         $mail->content_type = $message->getHeaderValue('Content-Type');;
         $mail->save();
     }
