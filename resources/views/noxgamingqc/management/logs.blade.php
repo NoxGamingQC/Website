ab5461c43721d29f9f4e22e5f2b948492b4a49a6
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-25 23:21:43 -0500

    Fix layouts

--- resources/views/noxgamingqc/management/logs.blade.php
+++ resources/views/noxgamingqc/management/logs.blade.php
@@ -1,55 +1,64 @@
-@extends('layouts.app')
+@extends('layouts.noxgamingqc.app')
 @section('title', trans('general.logs'))
 @section('content')
+
+
 <div class="row">
-    <div class="col-md-12">
-        <form action="/{{app()->getLocale()}}/management/logs">
-            <div class="input-group date" data-provide="datepicker">
-                <input class="form-control" type="date" name="date" value="{{ $date ? $date->format('Y-m-d') : today()->format('Y-m-d') }}" />
-                <div class="input-group-btn">
-                    <input type="submit" class="btn btn-primary" type="submit" value="{{trans('general.generate')}}">
+    <div class="col-md-12 content-item">
+        <div class="container-fluid">
+            <form action="/{{app()->getLocale()}}/management/logs">
+                <div class="input-group date" data-provide="datepicker">
+                    <input class="form-control" type="date" name="date" value="{{ $date ? $date->format('Y-m-d') : today()->format('Y-m-d') }}" />
+                    <div class="input-group-btn">
+                        <input type="submit" class="btn btn-primary" type="submit" value="{{trans('general.generate')}}">
+                    </div>
                 </div>
-            </div>
-        </form>
-        <span class="warning-text">{{trans('general.logs_limit')}}</span>
-        <hr />
-        @if (empty($data['file']))
-            <div>
-                <h3>{{trans('general.no_logs_found')}}</h3>
-            </div>
-        @else
-            <div class="col-md-6">
-                <br />
-                <h5>{{trans('general.updated_on')}} <b>{{ $data['lastModified']->format('Y-m-d h:i a') }}</b></h5>
-                <h5>{{trans('general.file_size')}} <b>{{ round($data['size'] / 1024)}} KB</b></h5>
-            </div>
-            <div class="col-md-6 text-right">
-                <form action="/{{app()->getLocale()}}/management/logs/download">
-                    <input class="hidden" type="date" name="date" value="{{ $date ? $date->format('Y-m-d') : today()->format('Y-m-d') }}" />
-                    <button class="btn btn-primary text-right" type="submit"><i id="navSearchButton" class="fa fa-download"></i> {{trans('general.download')}}</button>
-                </form>
-            </div>
-            <div class="col-md-12 table-responsive">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">date</th>
-                            <th scope="col">status</th>
-                            <th scope="col">data</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($data['file'] as $key => $value)
-                        <tr>
-                            <th scope="row">{{array_key_exists($key, $data['elementsDates']) ? $data['elementsDates'][$key] : ''}}</th>
-                            <td><p class="badge-{{array_key_exists($key, $data['elementsStatusColor']) ? $data['elementsStatusColor'][$key] : ''}}">{{array_key_exists($key, $data['elementsStatus']) ? $data['elementsStatus'][$key] : ''}}</p></td>
-                            <td>{!!$value!!}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-        @endif
+            </form>
+            <span class="warning-text">{{trans('general.logs_limit')}}</span>
+        </div>
     </div>
 </div>
-@stop
+<div class="row bg-dark">
+    <div class="col-md-12 content-item">
+        <div class="container-fluid">
+            @if (empty($data['file']))
+                <div>
+                    <h3 class="text-center">{{trans('general.no_logs_found')}}</h3>
+                </div>
+            @else
+                <div class="col-md-6">
+                    <br />
+                    <h5>{{trans('general.updated_on')}} <b>{{ $data['lastModified']->format('Y-m-d h:i a') }}</b></h5>
+                    <h5>{{trans('general.file_size')}} <b>{{ round($data['size'] / 1024)}} KB</b></h5>
+                </div>
+                <div class="col-md-6 text-right">
+                    <form action="/{{app()->getLocale()}}/management/logs/download">
+                        <input class="hidden" type="date" name="date" value="{{ $date ? $date->format('Y-m-d') : today()->format('Y-m-d') }}" />
+                        <button class="btn btn-primary text-right" type="submit"><i id="navSearchButton" class="fa fa-download"></i> {{trans('general.download')}}</button>
+                    </form>
+                </div>
+                <div class="col-md-12 table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">date</th>
+                                <th scope="col">status</th>
+                                <th scope="col">data</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($data['file'] as $key => $value)
+                            <tr>
+                                <th scope="row">{{array_key_exists($key, $data['elementsDates']) ? $data['elementsDates'][$key] : ''}}</th>
+                                <td><p class="badge-{{array_key_exists($key, $data['elementsStatusColor']) ? $data['elementsStatusColor'][$key] : ''}}">{{array_key_exists($key, $data['elementsStatus']) ? $data['elementsStatus'][$key] : ''}}</p></td>
+                                <td>{!!$value!!}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @endif
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/noxgamingqc/management/modules.blade.php
+++ resources/views/noxgamingqc/management/modules.blade.php
@@ -1,46 +1,43 @@
-@extends('layouts.app')
+@extends('layouts.noxgamingqc.app')
 @section('title', 'Management - Modules')
 @section('content')
 
-<div class="row">
+<div class="row bg-dark" style="padding-top:10%;padding-bottom:10%">
     <div class="col-md-12">
-        <h1>Modules</h1>
-        <hr />
-            @foreach($modules as $module)
-                <div class="col-md-3">
-                    <div class="panel panel-primary">
-                        <div class="panel-body">
-                            <div class="text-center">
-                                <h4><b>{{ $module['icon'] }} {{$module['slug']}}</b></h4>
-                                <br />
+        <div class="container-fluid">
+            @foreach($modules as $key => $module)
+                <div class="col-md-4">
+                    <div class="text-center">
+                        <h4><b>{{ $module['icon'] }} {{$module['slug']}}</b></h4>
+                        <br />
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="col-md-2">
+                                <label>Module Status</label>
                             </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="col-md-2">
-                                        <label>Module Status</label>
-                                    </div>
-                                    <div class="col-md-1">
-                                        <select class="selectpicker">
-                                            <option {{ !$module['isInMaintenance'] ? 'selected' : '' }}>Active</option>
-                                            <option {{ $module['isInMaintenance'] ? 'selected' : '' }}>Disable (In maintenance)</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="col-md-2">
-                                        <label>Default Status</label>
-                                    </div>
-                                    <div class="col-md-1">
-                                        <select class="selectpicker">
-                                            <option {{ $module['isActiveDefault'] ? 'selected' : '' }}>Active</option>
-                                            <option {{ !$module['isActiveDefault'] ? 'selected' : '' }}>Disable</option>
-                                        </select>
-                                    </div>
-                                </div>
+                            <div class="col-md-1">
+                                <select class="selectpicker">
+                                    <option {{ !$module['isInMaintenance'] ? 'selected' : '' }}>Active</option>
+                                    <option {{ $module['isInMaintenance'] ? 'selected' : '' }}>Disable (In maintenance)</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="col-md-2">
+                                <label>Default Status</label>
+                            </div>
+                            <div class="col-md-1">
+                                <select class="selectpicker">
+                                    <option {{ $module['isActiveDefault'] ? 'selected' : '' }}>Active</option>
+                                    <option {{ !$module['isActiveDefault'] ? 'selected' : '' }}>Disable</option>
+                                </select>
                             </div>
                         </div>
                     </div>
                 </div>
             @endforeach
+        </div>
     </div>
-@stop
+</div>
+@endsection
