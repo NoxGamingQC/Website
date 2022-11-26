096d79216c30db705ab5e08b76b55fa705e18ebf
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-25 22:20:04 -0500

    Fix 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,7 +8,7 @@
     <div class="col-md-12 bg-dark content-item">
         <div class="container">
             <h3 class="raleway-font text-danger">ERROR 404 - PAGE NOT FOUND</h3>
-            <p class="raleway-font text-danger">The page you are requesting doesn't exist? Check the route on top of the browser. Did you do a typo? Is it an old link try to use the navigation tab on top instead it might help.</p>
+            <p class="raleway-font text-danger">The page you are requesting doesn't exist? Check the route on top of the browser. Did you do a typo? Is it an old link? Try to use the navigation tab on top instead it might help.</p>
         </div>
     </div>
 </div>
