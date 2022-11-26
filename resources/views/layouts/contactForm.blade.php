130404340fc5ef70857e845f77bc071420d667e5
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-25 22:18:15 -0500

    Fix form

--- resources/views/layouts/contactForm.blade.php
+++ resources/views/layouts/contactForm.blade.php
@@ -1,40 +1,45 @@
-<div class="row">
-    <div class="col-md-6 text-center">
-        <div style="margin:5%;margin-top: 15%">
-            <h5 class="raleway-font"><a class="text-color" href="mailto:nox@noxgamingqc.ca">jbedard@noxgamingqc.ca</a></h5>
-            <h5 class="raleway-font"><a class="text-color" href="tel:819-852-8705">819-852-8705</a></h5>
-            <br />
-            <p>
-                @include('layouts.socials')
-            </p>
+
+<div class="row bg-dark">
+    <div class="col-md-12 content-item" id="socialNetwork">
+        <div class="container-fluid">
+            <div class="col-md-6 text-center">
+                <div style="margin:5%;margin-top: 15%">
+                    <h5 class="raleway-font"><a class="text-color" href="mailto:nox@noxgamingqc.ca">jbedard@noxgamingqc.ca</a></h5>
+                    <h5 class="raleway-font"><a class="text-color" href="tel:819-852-8705">819-852-8705</a></h5>
+                    <br />
+                    <p>
+                        @include('layouts.socials')
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6">
+            <form class="form-horizontal" action="/contact" method="post">
+                    <div class="form-group">
+                        <label class="control-label">{{trans('contact.name')}} <i class="text-danger">*</i> </label>
+                        <input id="contactName" type="text" class="form-control" placeholder="{{trans('contact.enter_name')}}">
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">{{trans('contact.email')}} <i class="text-danger">*</i> </label>
+                        <input id="contactEmail" type="text" class="form-control" placeholder="{{trans('contact.enter_email')}}">
+                        <p id="contactEmailNotValid" class="text-danger hidden" hidden> Please enter a valid email address</p>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">{{trans('contact.object')}} </label>
+                        <input id="contactObject" type="text" class="form-control" placeholder="{{trans('contact.enter_object')}}">
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">{{trans('contact.message')}} <i class="text-danger">*</i> </label>
+                        <textarea id="contactMessage" type="text" class="form-control" rows="4" placeholder="{{trans('contact.enter_message')}}"></textarea>
+                    </div>
+                    <div>
+                        {{trans('contact.required')}}<i class="text-danger">*</i>
+                    </div>
+                    <div class="text-center">
+                        <button type="button" id="submitContactForm" class="btn btn-success">{{trans('general.send')}}</button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
-    <div class="col-md-6">
-    <form class="form-horizontal" action="/contact" method="post">
-            <div class="form-group">
-                <label class="control-label">{{trans('contact.name')}} <i class="text-danger">*</i> </label>
-                <input id="contactName" type="text" class="form-control" placeholder="{{trans('contact.enter_name')}}">
-            </div>
-            <div class="form-group">
-                <label class="control-label">{{trans('contact.email')}} <i class="text-danger">*</i> </label>
-                <input id="contactEmail" type="text" class="form-control" placeholder="{{trans('contact.enter_email')}}">
-                <p id="contactEmailNotValid" class="text-danger hidden" hidden> Please enter a valid email address</p>
-            </div>
-            <div class="form-group">
-                <label class="control-label">{{trans('contact.object')}} </label>
-                <input id="contactObject" type="text" class="form-control" placeholder="{{trans('contact.enter_object')}}">
-            </div>
-            <div class="form-group">
-                <label class="control-label">{{trans('contact.message')}} <i class="text-danger">*</i> </label>
-                <textarea id="contactMessage" type="text" class="form-control" rows="4" placeholder="{{trans('contact.enter_message')}}"></textarea>
-            </div>
-            <div>
-                {{trans('contact.required')}}<i class="text-danger">*</i>
-            </div>
-            <div class="text-center">
-                <button type="button" id="submitContactForm" class="btn btn-success">{{trans('general.send')}}</button>
-            </div>
-        </form>
     </div>
 </div>
 
