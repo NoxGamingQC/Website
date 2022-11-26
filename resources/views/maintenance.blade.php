0dbce5e96092ece7cfdc5d3da3f931c613f27324
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-25 22:25:36 -0500

    Fix maintenance page

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -15,9 +15,8 @@
     </div>
 </nav>
 <br />
-<br />
 
-<div class="header" style="height:600px !important;">
+<div class="header" style="height:700px !important;">
     <div class="row" style="padding-top: 100px">
         <div class="col-md-12 text-center">
             <h1 class="text-highlight">{{trans('maintenance.error')}}</h1>
@@ -36,13 +35,13 @@
             <br />
             <br />
             <div class="text center">
-                <a id="frLink" class="text-highlight hidden" href="/fr/maintenance"> Français ({{trans("general.french")}})</a>
-                <a id="enLink" class="text-highlight hidden" href="/en/maintenance"> English ({{trans("general.english")}})</a>
+                <a id="frLink" class="text-color hidden" href="/fr/maintenance"> Français ({{trans("general.french")}})</a>
+                <a id="enLink" class="text-color hidden" href="/en/maintenance"> English ({{trans("general.english")}})</a>
             </div>
             @auth
                 <hr />
                 <div class="col-md-10">
-                    <p class="text-highlight">{{trans('maintenance.already_logged_in')}} <a class="text-highlight" href="/{{app()->getLocale()}}/home">{{trans('maintenance.travel_to_home_page')}}</a></p>
+                    <p class="text-highlight">{{trans('maintenance.already_logged_in')}} <a class="text-color" href="/{{app()->getLocale()}}/home">{{trans('maintenance.travel_to_home_page')}}</a></p>
                 </div>
                 <div class="col-md-2 text-right">
                     <a class="btn btn-default text-highlight" href="/logout"> {{trans('general.logout')}}</a>
