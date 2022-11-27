32eab3e9bd9bef88637397250beed258233d9daf
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-27 13:49:59 -0500

    Fix

--- resources/views/layouts/noxgamingqc/header.blade.php
+++ resources/views/layouts/noxgamingqc/header.blade.php
@@ -1,10 +1,8 @@
-<div class="text-center" id="title">
-    <div class="header">
-        <div class="row">
-            <h1 class="text-highlight">@yield('title')</h1>
-            <br />
-            <h3 class="raleway-font text-highlight" style="margin-left:5%;max-width:90%">@yield('slogan')</h3>
-            <br />
-        </div>
+<div class="header">
+    <div class="row text-center">
+        <h1 class="text-highlight">@yield('title')</h1>
+        <br />
+        <h3 class="raleway-font text-highlight" style="margin-left:5%;max-width:90%">@yield('slogan')</h3>
+        <br />
     </div>
 </div>
