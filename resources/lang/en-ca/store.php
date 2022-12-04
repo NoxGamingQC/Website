f8054ea1f8b99254492ccc973fe2d5fac831b0ce
NoxGamingQC <noxgamingqc@gmail.com>
2022-12-04 13:29:04 -0500

    Add sort

--- resources/lang/en-ca/store.php
+++ resources/lang/en-ca/store.php
@@ -4,5 +4,13 @@ return [
     'variable_price' => "Variable price",
     'price_not_available' => "Price unavailable",
     'out_of_stock' => "Out of stock",
-    'low_stock' => "Low stock"
+    'low_stock' => "Low stock",
+    'no_displayed_item' => "No item to display",
+    'consoles' => "Consoles",
+    'games' => "Games",
+    'clothes' => "Clothes",
+    'car' => "Car",
+    'other' => "Other",
+    'computer_science' => "Computer / Tech",
+    'landscaping' => "Landscaping"
 ];
--- resources/lang/fr-ca/store.php
+++ resources/lang/fr-ca/store.php
@@ -4,5 +4,14 @@ return [
     'variable_price' => "Prix variable",
     'price_not_available' => "Prix indisponible",
     'out_of_stock' => "Rupture de stock",
-    'low_stock' => "Stock faible"
+    'low_stock' => "Stock faible",
+    'no_displayed_item' => "Aucun article à afficher",
+    'consoles' => "Consoles",
+    'games' => "Jeux",
+    'clothes' => "Vêtements",
+    'car' => "Véhicule",
+    'other' => "Autre",
+    'computer_science' => "Informatique",
+    'landscaping' => "Entretien paysager"
+
 ];
