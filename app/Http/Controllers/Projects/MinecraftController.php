c8667dec50d0ef565fecc802c749b8d90fcc6092
NoxGamingQC <noxgamingqc@gmail.com>
2023-01-25 17:53:56 -0500

    Adding minecraft section in projects

---
+++ app/Http/Controllers/Projects/MinecraftController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Projects;
+
+use Auth;
+use DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MinecraftController extends Controller
+{
+    public function index()
+    {
+        abort(503);
+    }
+}
