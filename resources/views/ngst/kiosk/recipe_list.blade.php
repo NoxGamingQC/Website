ec9e0c0065768d50f4a96717692dfee95fe39ce1
NoxGamingQC <noxgamingqc@gmail.com>
2023-02-05 13:07:18 -0500

    Fix layout of cookbook

--- resources/views/ngst/kiosk/recipe_list.blade.php
+++ resources/views/ngst/kiosk/recipe_list.blade.php
@@ -3,11 +3,11 @@
 @section('title','Mon livre de recettes')
 @section('slogan', 'En dévelopement')
 
-<div class="container-fluid">
+<div class="container">
     <div class="row">
-        <div class="col-md-12 text-center">
-            <h3>Déjeuner</h3>
-            <ul>
+        <div class="col-md-12 text-left">
+            <h2>Déjeuner</h2>
+            <ul style="font-size:24px">
                 <li><a href="/company/kiosk/recipe/pancakes">Pancakes</a></li>
             </ul>
         </div>
