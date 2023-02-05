f97ff05b2947b69a187a4dde0337962fff8303b3
NoxGamingQC <noxgamingqc@gmail.com>
2023-02-05 12:25:49 -0500

    Change kiosk to display recipe

---
+++ resources/views/ngst/kiosk/recipe_list.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.kiosk')
+@section('content')
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1>Mon livre de recettes</h1>
+        </div>
+    </div>
+</div>
+
+@stop
