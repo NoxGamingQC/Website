84da951a964c9eff33d8cffe7747860aef737329
NoxGamingQC <noxgamingqc@gmail.com>
2023-02-05 12:29:11 -0500

    fix main page

--- resources/views/ngst/kiosk/recipe_list.blade.php
+++ resources/views/ngst/kiosk/recipe_list.blade.php
@@ -1,12 +1,12 @@
-@extends('layouts.kiosk')
+@extends('layouts.noxgamingqc.app')
 @section('content')
+@section('title','Mon livre de recettes')
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12 text-center">
-            <h1>Mon livre de recettes</h1>
         </div>
     </div>
 </div>
 
-@stop
+@endsection
