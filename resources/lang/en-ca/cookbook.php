2be1ef95459e005b4f61297b961ed486807059d8
NoxGamingQC <noxgamingqc@gmail.com>
2023-02-06 15:00:00 -0500

    Add language support

---
+++ resources/lang/en-ca/cookbook.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => "My cookbook",
+    'slogan' => "In development",
+    'french' => "Voir en Français (French)",
+    'english' => "See in english (English)",
+    'breakfast' => "Breakfast",
+    'lunch' => "Lunch",
+    'dessert' => "Dessert",
+    'crepes' => "Crepes",
+    'pancakes' => "Pancakes"
+];
---
+++ resources/lang/fr-ca/cookbook.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "Mon livre de recettes",
+    'slogan' => "En dévelopementt",
+    'breakfast' => "Déjeuner",
+    'lunch' => "Repas",
+    'dessert' => "Dessert",
+    'crepes' => "Crêpes",
+    'pancakes' => "Pancakes"
+];
