d479238dd09d1debb8a23f48d1ad8e37906fa1e8
NoxGamingQC <noxgamingqc@gmail.com>
2023-02-10 18:18:16 -0500

    Add drinks

---
+++ resources/views/ngst/kiosk/cookbook/recipe/drinks/daquiri.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.noxgamingqc.app')
+@section('content')
+@section('title','Daquiri')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-right">
+            <br />
+            <a href="/{{app()->getLocale()}}/kiosk/cookbook/drinks"><input class="btn btn-primary form-control" style="font-size:24px;padding:5%" value="{{trans('cookbook.go_back_to_list')}}" readonly></a>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-left">
+            <h2>Ingredients</h2>
+            <br />
+            <ul style="font-size:24px">
+                <li>4oz de rhum blanc</li>
+                <li>2oz de jus de lime</li>
+                <li>1oz de sirop simple</li>
+                <li>2 rondelles de lime ou citron</li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <br />
+            &nbsp
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-left">
+            <h2>Directives</h2>
+            <br />
+            <ul style="font-size:24px">
+                <li>Servir dans un verre à Martini</li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <br />
+            &nbsp
+        </div>
+    </div>
+</div>
+
+@endsection
