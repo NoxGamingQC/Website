b983f23b88aadef5595809fc330f67dc59b45158
NoxGamingQC <noxgamingqc@gmail.com>
2023-03-02 20:28:04 -0500

    Fix display layout

--- resources/views/noxgamingqc/profile/mail.blade.php
+++ resources/views/noxgamingqc/profile/mail.blade.php
@@ -1,26 +1,8 @@
 @extends('layouts.noxgamingqc.app')
 @section('title', trans('general.mail'))
-@section('slogan', $mail->object)
 @section('header', false)
 @section('content')
 
-<div class="row">
-    <div class="col-md-12 content-item bg-dark">
-        <div class="container">
-            <div class="col-md-6">
-                <p><b><u>Sender name:</u></b> {{$mail->sender_name}}</p>
-                <p><b><u>From:</u></b> {{$mail->sender}}</p>
-                <p><b><u>To:</u></b> {{$mail->recipient}}</p>
-                <p><b><u>Received on:</u></b> {{Carbon\Carbon::parse($mail->created_at)->setTimezone('America/Toronto')->format('Y-m-d')}}</p>
-            </div>
-            <div class="col-md-6 text-right">
-                <a class="btn btn-primary" href="/{{app()->getLocale()}}/profile/mail"><i class="fa fa-reply" area-hidden="true"></i></a>
-                <a class="btn btn-danger text-color" href="/{{app()->getLocale()}}/profile/mail/{{$mail->id}}/delete"><i class="fa fa-trash"></i></a>
-                <a class="btn btn-primary" href="/{{app()->getLocale()}}/profile/mail">Go back</a>
-            </div>
-        </div>
-    </div>
-</div>
 <div class="row">
     <div class="col-md-12">
         <br />
@@ -47,15 +29,22 @@
                                 <img class="img-circle" src="/img/no-avatar.jpg" width="100%">
                             </div>
                             <div class="col-md-10">
-                                &nbsp
-                                <p style="overflow:visible">{{$mail->sender_name}} &lt;{{$mail->sender}}&gt;</p>
-                                <p><small>À : {{$mail->recipient}}</small></p>
+                                <h4 class="raleway-font" style="overflow:visible">{{$mail->sender_name}} &lt;{{$mail->sender}}&gt;</h4>
+                                <p>À : {{$mail->recipient}}</p>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-4 text-right">
-                        &nbsp
-                        <p>{{Carbon\Carbon::parse($mail->created_at)->setTimezone('America/Toronto')->format('D Y-m-d H:i')}}</p>
+                        <div class="col-md-12">
+                            <a class="btn btn-primary" href="#"><i class="fa fa-reply" area-hidden="true"></i></a>
+                            <a class="btn btn-primary" href="#"><i class="fa fa-reply-all" area-hidden="true"></i></a>
+                            <a class="btn btn-primary" href="#"><i class="fa fa-share" area-hidden="true"></i></a>
+                            <a class="btn btn-danger text-color" href="/{{app()->getLocale()}}/profile/mail/{{$mail->id}}/delete"><i class="fa fa-trash"></i></a>
+                        </div>
+                        <div class="col-md-12">
+                            &nbsp
+                            <p>{{Carbon\Carbon::parse($mail->created_at)->setTimezone('America/Toronto')->format('D Y-m-d H:i')}}</p>
+                        </div>
                     </div>
                     <div class="col-md-12">
                         &nbsp
