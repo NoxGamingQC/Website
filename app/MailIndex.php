bc69ef3f121ba109aeb27ed1ecf596ba93ef1ad8
NoxGamingQC <noxgamingqc@gmail.com>
2023-03-04 15:06:09 -0500

    testing receive email

---
+++ app/MailIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MailIndex extends Model
+{
+    protected $table = 'mail_index';
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+}
