a40d36beaf3c5154c91fe3024be516d7bdcb11d5
NoxGamingQC <noxgamingqc@gmail.com>
2023-03-14 17:20:45 -0400

    Pre make template

---
+++ resources/views/ngst/kiosk/cookbook/recipe.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.noxgamingqc.app')
+@section('content')
+@section('name','Crêpes')
+@section('author', 'Lady Alie')
+@section('prep_time', trans('cookbook.not_available'))
+@section('cook_time', trans('cookbook.not_available'))
+@section('yields', '12-15 crêpes')
+@section('description', trans('cookbook.not_available'))
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-left">
+            <h2>Ingredients</h2>
+            <br />
+            <ul style="font-size:18px">
+                <li>1 1/2 tasse de lait</li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <br />
+            &nbsp
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-left">
+            <h2>Directives</h2>
+            <br />
+            <ul style="font-size:18px">
+                <li class="text-justify">Chauffer une poêle à feu moyen. Lorsque la poêle est chaude, badigeonner de beurre.</li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <br />
+            &nbsp
+        </div>
+    </div>
+</div>
+
+@endsection
