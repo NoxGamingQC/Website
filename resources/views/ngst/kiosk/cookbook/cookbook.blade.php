ca1a5487c2c762ebabd526610057c1f226133e27
NoxGamingQC <noxgamingqc@gmail.com>
2023-03-17 12:39:48 -0400

    Fixing key

--- resources/views/ngst/kiosk/cookbook/cookbook.blade.php
+++ resources/views/ngst/kiosk/cookbook/cookbook.blade.php
@@ -8,9 +8,9 @@
         <div class="col-md-12">
             <br />
             @if(app()->getLocale() == 'fr-ca')
-                <a href="/en-ca/kiosk/cookbook"><input class="btn btn-primary form-control" style="font-size:18px;padding:5%" value="{{trans('cookbook.english')}}" readonly></a>
+                <a href="/en-ca/kiosk/cookbook{{isset($kiosk_key) ? '?kiosk_key=' . $kiosk_key : ''}}"><input class="btn btn-primary form-control" style="font-size:18px;padding:5%" value="{{trans('cookbook.english')}}" readonly></a>
             @elseif (app()->getLocale() == 'en-ca')
-                <a href="/fr-ca/kiosk/cookbook"><input class="btn btn-primary form-control" style="font-size:18px;padding:5%" value="{{trans('cookbook.french')}}" readonly></a>
+                <a href="/fr-ca/kiosk/cookbook{{isset($kiosk_key) ? '?kiosk_key=' . $kiosk_key : ''}}"><input class="btn btn-primary form-control" style="font-size:18px;padding:5%" value="{{trans('cookbook.french')}}" readonly></a>
             @endif
             <br />
             &nbsp
@@ -22,13 +22,13 @@
         @if(app()->getLocale() === 'fr-ca')
             @foreach($categories as $key => $category)
                 <div class="col-sm-4 text-center" style="margin-bottom:3%">
-                    <a href="/{{app()->getLocale()}}/kiosk/cookbook/{{$category->id}}"><input class="btn btn-primary form-control" style="font-size:18px;padding:15%" value="{{$category->name_fr}}" readonly></a>
+                    <a href="/{{app()->getLocale()}}/kiosk/cookbook/{{$category->id}}{{isset($kiosk_key) ? '?kiosk_key=' . $kiosk_key : ''}}"><input class="btn btn-primary form-control" style="font-size:18px;padding:15%" value="{{$category->name_fr}}" readonly></a>
                 </div>
             @endforeach
         @else
             @foreach($categories as $key => $category)
                 <div class="col-sm-4 text-center" style="margin-bottom:3%">
-                    <a href="/{{app()->getLocale()}}/kiosk/cookbook/{{$category->id}}"><input class="btn btn-primary form-control" style="font-size:18px;padding:15%" value="{{$category->name_en}}" readonly></a>
+                    <a href="/{{app()->getLocale()}}/kiosk/cookbook/{{$category->id}}{{isset($kiosk_key) ? '?kiosk_key=' . $kiosk_key : ''}}"><input class="btn btn-primary form-control" style="font-size:18px;padding:15%" value="{{$category->name_en}}" readonly></a>
                 </div>
             @endforeach
         @endif
