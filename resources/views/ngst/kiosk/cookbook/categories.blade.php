bfae1bf9e8a3fb04c35d052bc9b8c702da4935fa
NoxGamingQC <noxgamingqc@gmail.com>
2023-03-17 12:41:09 -0400

    Fix key

--- resources/views/ngst/kiosk/cookbook/categories.blade.php
+++ resources/views/ngst/kiosk/cookbook/categories.blade.php
@@ -26,13 +26,13 @@
             @if(app()->getLocale() === 'fr-ca')
                 @foreach($recipes as $key => $recipe)
                     <div class="col-sm-4 text-center" style="margin-bottom:3%">
-                        <a href="/{{app()->getLocale()}}/kiosk/recipe/{{$recipe->id}}"><input class="btn btn-primary form-control" style="font-size:18px;padding:15%" value="{{$recipe->name_fr}}" readonly></a>
+                        <a href="/{{app()->getLocale()}}/kiosk/recipe/{{$recipe->id}}{{isset($kiosk_key) ? '?kiosk_key=' . $kiosk_key : ''}}"><input class="btn btn-primary form-control" style="font-size:18px;padding:15%" value="{{$recipe->name_fr}}" readonly></a>
                     </div>
                 @endforeach
             @else
                 @foreach($recipes as $key => $recipe)
                     <div class="col-sm-4 text-center" style="margin-bottom:3%">
-                        <a href="/{{app()->getLocale()}}/kiosk/recipe/{{$recipe->id}}"><input class="btn btn-primary form-control" style="font-size:18px;padding:15%" value="{{$recipe->name_en}}" readonly></a>
+                        <a href="/{{app()->getLocale()}}/kiosk/recipe/{{$recipe->id}}{{isset($kiosk_key) ? '?kiosk_key=' . $kiosk_key : ''}}"><input class="btn btn-primary form-control" style="font-size:18px;padding:15%" value="{{$recipe->name_en}}" readonly></a>
                     </div>
                 @endforeach
             @endif
