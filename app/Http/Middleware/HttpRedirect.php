5ebd8b8044d183eb7c1896cd3941e7eaf339d8b9
NoxGamingQC <noxgamingqc@gmail.com>
2023-03-18 12:25:30 -0400

    Redirect

--- app/Http/Middleware/HttpRedirect.php
+++ app/Http/Middleware/HttpRedirect.php
@@ -18,7 +18,7 @@ class HttpRedirect
     public function handle(Request $request, Closure $next)
     {
         if ((!$request->secure() || $_SERVER['REQUEST_SCHEME'] == 'http') && App::environment('production') && ('http://' . $_SERVER['HTTP_HOST'] !== env('APP_URL'))) {
-            return redirect()->secure($request->getRequestUri());
+            return redirect()->secure($request->getRequestUri(), 301);
         }
 
         return $next($request);
