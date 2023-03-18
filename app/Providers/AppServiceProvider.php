5424cc3e519c24b0831f90eb8420642eba97d699
NoxGamingQC <noxgamingqc@gmail.com>
2023-03-17 23:50:09 -0400

    Trying to fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if(env('APP_URL') !== 'http://localhost:8000') {
-            \URL::forceScheme('https');
+           // \URL::forceScheme('https');
         }
 
         $mainConfig = MainConfig::all();
