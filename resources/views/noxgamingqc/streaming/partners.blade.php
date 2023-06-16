03a4c8ea9f298f221e8134d5c52c12986b801a70
NoxGamingQC <noxgamingqc@gmail.com>
2023-06-16 19:07:29 -0400

    Changing size of pannel

--- resources/views/noxgamingqc/streaming/partners.blade.php
+++ resources/views/noxgamingqc/streaming/partners.blade.php
@@ -14,14 +14,14 @@
                     <div class="row">
                         <div class="col-md-offset-1 col-md-5">
                             <div class="panel panel-primary text-center">
-                                <div class="panel-body img" style="background-image: url('/img/Partners/CorpsRoyaux.png') !important; background-size: cover !important; height: 350px !important;background-position:center center">
+                                <div class="panel-body img" style="background-image: url('/img/Partners/CorpsRoyaux.png') !important; background-size: cover !important; height: 200px !important;background-position:center center">
                                     <h3 class="img-text"><b>Corps Royaux</b></h3>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-5">
                             <div class="panel panel-primary text-center">
-                                <div class="panel-body img" style="background-image: url('/img/Partners/Instant-Gaming.png') !important; background-size: cover !important; height: 350px !important;background-position:center center">
+                                <div class="panel-body img" style="background-image: url('/img/Partners/Instant-Gaming.png') !important; background-size: cover !important; height: 200px !important;background-position:center center">
                                     <h3 class="img-text"><b>Instant Gaming</b></h3>
                                 </div>
                             </div>
@@ -42,14 +42,14 @@
                     <div class="row">
                         <div class="col-md-5 col-md-offset-1">
                             <div class="panel panel-primary text-center">
-                                <div class="panel-body extra-life-logo" style="height: 350px !important; background-position:center center">
+                                <div class="panel-body extra-life-logo" style="height: 200px !important; background-position:center center">
                                     <h3 class="img-text"><b>Extra Life</b></h3>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-5">
                             <div class="panel panel-primary text-center">
-                                <div class="panel-body img" style="background-image: url('/img/Affiliates/HumbleBundle.png') !important; background-size: cover !important; height: 350px !important; background-position:center center">
+                                <div class="panel-body img" style="background-image: url('/img/Affiliates/HumbleBundle.png') !important; background-size: cover !important; height: 200px !important; background-position:center center">
                                     <h3 class="img-text"><b>Humble Bundle</b></h3>
                                 </div>
                             </div>
