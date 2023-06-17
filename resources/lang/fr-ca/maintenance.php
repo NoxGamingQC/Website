f34fb0f755517030ec2949ba8cba4465500dcd6c
NoxGamingQC <noxgamingqc@gmail.com>
2023-06-16 23:46:52 -0400

    Fix maintenance text

--- resources/lang/fr-ca/maintenance.php
+++ resources/lang/fr-ca/maintenance.php
@@ -3,9 +3,9 @@
 return [
     'sorry' => "Désolé",
     'under_maintenance' => "En maintenance",
-    'explanation' => "Le site web est sous maintenance temporairement. Merci de votre patience.",
+    'explanation' => "Notre site web est présentement sous maintenance et reviendras en ligne sous peu. Merci de votre patience.",
     'dev_only' => "P.S. Seul les membres ayant les droits développeur ont accès au site web pour le moment.",
-    'already_logged_in' => "¯\_(ツ)_/¯ Vous êtes connecter. Si vous n'avez pas accès au site web, prenez quelques minutes pour lire le message ci-haut. Si vous n'avez pas été redirigé, voici un lien vers la page d'accueil: ",
+    'already_logged_in' => "Vous êtes connecté. Vous pouvez essayé de rejoindre la page d'accueil, mais si vous êtes toujours ici, il y a des chances que vous n'ayez pas les droits requis pour accéder au site web en ce moment. Si vous voulez tout de même essayé, voici un lien pour retourner à la page d'accueil: ",
     'travel_to_home_page' => "Allez vers la page d'accueil",
     'maintenance_text' => "Désolé pour tout les inconvéniants.",
     'error'=> "En maintenance"
