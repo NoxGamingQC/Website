893d6278339d46b7a87f389c31382e01e914dd0a
NoxGamingQC <noxgamingqc@gmail.com>
2023-06-17 08:07:01 -0400

    Semi-fix themes

--- resources/views/layouts/noxgamingqc/footer.blade.php
+++ resources/views/layouts/noxgamingqc/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="row" style="background:linear-gradient(rgba(5, 5, 5, 1),rgba(0, 0, 0, 0.9)),url('/img/NoxGamingQC.png');background-position: 50% 65%;background-repeat: no-repeat;background-size: cover; padding: 5%;padding-top:3%;padding-bottom:3%; margin-top:3%">
+<div class="row footer">
     <div class="col-md-12 text-center">
         <p>
             @include('layouts.socials')
