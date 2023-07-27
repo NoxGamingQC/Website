90d2fb43fcc89bda2a9704e04064dd9d2a7a15fa
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-26 22:25:53 -0400

    Regenerating composer.lock

--- resources/views/layouts/ngst/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="row" style="margin:5%;">
-    <div class="col-md-12 text-center">
-        <br />
-        <h5 class="raleway-font">{{trans('general.copyright_ngst')}}</h5>
-        <br />
-        <p><small>commit: {{$sourceVersion}}<small></p>
-    </div>
-</div>
