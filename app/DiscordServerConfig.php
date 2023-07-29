e8eed716a3dda907f8944ba9633ca2dcc01619ba
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-29 12:00:42 -0400

    Add update server method

---
+++ app/DiscordServerConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DiscordServerConfig extends Model
+{
+    protected $table = 'discord_server_config';
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+}
