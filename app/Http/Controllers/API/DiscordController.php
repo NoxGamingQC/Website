8e02a845d9042f36ebb011cad54dc0d60a5bf0e9
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-28 21:22:19 -0400

    Removing dd

--- app/Http/Controllers/API/DiscordController.php
+++ app/Http/Controllers/API/DiscordController.php
@@ -14,20 +14,22 @@ class DiscordController extends Controller
     public function update(Request $request) {
         $getApp = ApiKey::where('key', '=', $request->website_token)->first();
         if($getApp) {
-            foreach($request->users as $discordID => $discordUsername) {
-                $existingDiscordUser = DiscordUsers::where('discord_id', '=', $discordID)->first();
-                if($existingDiscordUser) {
-                    if($existingDiscordUser->name !== $discordUsername) {
-                        $existingDiscordUser->name = $discordUsername;
-                        $existingDiscordUser->save();
+            if($request->users) {
+                foreach($request->users as $discordID => $discordUsername) {
+                    $existingDiscordUser = DiscordUsers::where('discord_id', '=', $discordID)->first();
+                    if($existingDiscordUser) {
+                        if($existingDiscordUser->name !== $discordUsername) {
+                            $existingDiscordUser->name = $discordUsername;
+                            $existingDiscordUser->save();
+                            return;
+                        }
+                    } else {
+                        $discordUser = new DiscordUsers;
+                        $discordUser->discord_id = $discordID;
+                        $discordUser->name = $discordUsername;
+                        $discordUser->save();
                         return;
                     }
-                } else {
-                    $discordUser = new DiscordUsers;
-                    $discordUser->discord_id = $discordID;
-                    $discordUser->name = $discordUsername;
-                    $discordUser->save();
-                    return;
                 }
             }
         }
