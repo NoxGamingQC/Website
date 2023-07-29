d99d77a5503c77714194da87817c23c3d57ba3bf
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-29 01:19:21 -0400

    Switch to arrays

--- app/Http/Controllers/API/DiscordController.php
+++ app/Http/Controllers/API/DiscordController.php
@@ -18,19 +18,19 @@ class DiscordController extends Controller
                 foreach($request->users as $discordID => $discordUser) {
                     $existingDiscordUser = DiscordUsers::where('discord_id', '=', $discordID)->first();
                     if($existingDiscordUser) {
-                        if($existingDiscordUser->name !== $discordUser->username) {
-                            $existingDiscordUser->name = $discordUser->username;
+                        if($existingDiscordUser->name !== $discordUser['username']) {
+                            $existingDiscordUser->name = $discordUser['username'];
                             $existingDiscordUser->save();
                         }
-                        if($existingDiscordUser->avatar_url !== $discordUser->avatar_url) {
-                            $existingDiscordUser->avatar_url = $discordUser->avatar_url;
+                        if($existingDiscordUser->avatar_url !== $discordUser['avatar_url']) {
+                            $existingDiscordUser->avatar_url = $discordUser['avatar_url'];
                             $existingDiscordUser->save();
                         }
                         return;
                     } else {
                         $newDiscordUser = new DiscordUsers;
                         $newDiscordUser->discord_id = $discordID;
-                        $newDiscordUser->name = $discordUser->username;
+                        $newDiscordUser->name = $discordUser['username'];
                         $newDiscordUser->avatar_url = $discordUser->avatar_url;
                         $newDiscordUser->save();
                         return;
