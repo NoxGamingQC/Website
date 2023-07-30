3237cd3c2049cece8d4b8345828c39b8233ccf35
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-30 15:35:39 -0400

    Fix menu

--- resources/views/layouts/noxgamingqc/mobile/navbar.blade.php
+++ resources/views/layouts/noxgamingqc/mobile/navbar.blade.php
@@ -13,21 +13,21 @@
                 @guest
                     <div class="col-xs-2 col-xs-offset-2">
                 @endguest
-                        <li id="home" class="nav-home text-center" style="padding:5%"><a href="/{{app()->getLocale()}}/home"><i class="fa fa-home" aria-hidden="true"></i></a></li>
+                        <a class="text-color" href="/{{app()->getLocale()}}/home"><li id="home" class="nav-home text-center" style="padding:5%"><i class="fa fa-home" aria-hidden="true"></i></li></a>
                     </div>
                     <div class="col-xs-2">
-                        <li id="about_us" class="nav-about_us text-center" style="padding:5%"><a class="text-color" onclick="openNavMenu('#aboutMeMenu')"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+                        <a class="text-color" onclick="openNavMenu('#aboutMeMenu')"><li id="about_us" class="nav-about_us text-center" style="padding:5%"><i class="fa fa-user" aria-hidden="true"></i></li></a>
                     </div>
                     @auth
                     <div class="col-xs-2">
-                        <li id="premium" class="nav-premium text-center" style="padding:5%"><a class="text-color" onclick="openNavMenu('#premiumMenu')"><i class="fa fa-star" aria-hidden="true"></i></a></li>
+                        <a class="text-color" onclick="openNavMenu('#premiumMenu')"><li id="premium" class="nav-premium text-center" style="padding:5%"><i class="fa fa-star" aria-hidden="true"></i></li></a>
                     </div>
                     @endauth
                     <div class="col-xs-2">
-                        <li id="language" class="nav-language text-center" style="padding:5%"><a class="text-color" onclick="openNavMenu('#languageMenu')"><i class="fa fa-language" aria-hidden="true"></i></a></li>
+                        <a class="text-color" onclick="openNavMenu('#languageMenu')"><li id="language" class="nav-language text-center" style="padding:5%"><i class="fa fa-language" aria-hidden="true"></i></li></a>
                     </div>
                     <div class="col-xs-2">
-                        <li id="profile" class="nav-profile text-center" style="padding:5%"><a class="text-color" onclick="openNavMenu('#profileMenu')">
+                        <a class="text-color" onclick="openNavMenu('#profileMenu')"><li id="profile" class="nav-profile text-center" style="padding:5%">
                             @auth
                                 @if(Auth::user()->AvatarURL)
                                     <img class="img img-circle status-{{Auth::user()->status}}" src="{{Auth::user()->AvatarURL}}" width="36px" style="border-width: 2px;" />
@@ -38,7 +38,7 @@
                             @guest
                                 <i class="fa fa-user-circle-o" aria-hidden="true"></i>
                             @endguest
-                        </a></li>
+                        </li></a>
                     </div>
                     @auth
                         <div class="col-xs-1">
@@ -52,32 +52,32 @@
             </ul>
             <div class="col-md-12">
                 <ul id="aboutMeMenu" class="text-center hidden" hidden>
-                    <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="/{{app()->getLocale()}}/about_us/contact" class="{{($page_lists['contact_us']['inMaintenance']) ? 'hidden' : ''}}" style="font-size:14px"><i class="fa fa-address-book " aria-hidden="true"></i> {{trans('general.contact_us')}}</a></li>
-                    <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="/{{app()->getLocale()}}/about_us/games" class="{{($page_lists['games']['inMaintenance']) ? 'hidden' : ''}}" style="font-size:14px"><i class="fa fa-gamepad" aria-hidden="true"></i> {{ trans('general.games_list') }}</a></li>
-                    <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="/{{app()->getLocale()}}/about_us/partners" class=""><i class="fa fa-handshake-o" aria-hidden="true" style="font-size:14px"></i> {{ trans('general.partners') }}</a></li>
-                    <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="/{{app()->getLocale()}}/about_us/projects" class="{{($page_lists['projects']['inMaintenance']) ? 'hidden' : ''}}" style="font-size:14px"><i class="fa fa-heart" aria-hidden="true"></i> {{ trans('general.projects') }}</a></li>
-                    <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="/{{app()->getLocale()}}/about_us/teams" class="" style="font-size:14px"><i class="fa fa-users" aria-hidden="true"></i> {{ trans('general.teams') }}</a></li>
-                    <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="/{{app()->getLocale()}}/about_us/twitch" class="{{($page_lists['twitch']['inMaintenance']) ? 'hidden' : ''}}" style="font-size:14px"><i class="fa fa-twitch" aria-hidden="true"></i> {{ trans('general.twitch') }}</a></li>
-                    <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="/{{app()->getLocale()}}/about_us/youtube" class="{{($page_lists['youtube']['inMaintenance']) ? 'hidden' : ''}}" style="font-size:14px"><i class="fa fa-youtube-play" aria-hidden="true"></i> {{ trans('general.youtube') }}</a></li>
+                    <a class="text-color" href="/{{app()->getLocale()}}/about_us/contact" class="{{($page_lists['contact_us']['inMaintenance']) ? 'hidden' : ''}}" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><i class="fa fa-address-book " aria-hidden="true"></i> {{trans('general.contact_us')}}</li></a>
+                    <a class="text-color" href="/{{app()->getLocale()}}/about_us/games" class="{{($page_lists['games']['inMaintenance']) ? 'hidden' : ''}}" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><i class="fa fa-gamepad" aria-hidden="true"></i> {{ trans('general.games_list') }}</li></a>
+                    <a class="text-color" href="/{{app()->getLocale()}}/about_us/partners" class="" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><i class="fa fa-handshake-o" aria-hidden="true" style="font-size:14px"></i> {{ trans('general.partners') }}</a></li>
+                    <a class="text-color" href="/{{app()->getLocale()}}/about_us/projects" class="{{($page_lists['projects']['inMaintenance']) ? 'hidden' : ''}}" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><i class="fa fa-heart" aria-hidden="true"></i> {{ trans('general.projects') }}</li></a>
+                    <a class="text-color" href="/{{app()->getLocale()}}/about_us/teams" class="" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><i class="fa fa-users" aria-hidden="true"></i> {{ trans('general.teams') }}</li></a>
+                    <a class="text-color" href="/{{app()->getLocale()}}/about_us/twitch" class="{{($page_lists['twitch']['inMaintenance']) ? 'hidden' : ''}}" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><i class="fa fa-twitch" aria-hidden="true"></i> {{ trans('general.twitch') }}</li></a>
+                    <a class="text-color" href="/{{app()->getLocale()}}/about_us/youtube" class="{{($page_lists['youtube']['inMaintenance']) ? 'hidden' : ''}}" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><i class="fa fa-youtube-play" aria-hidden="true"></i> {{ trans('general.youtube') }}</li></a>
                 </ul>
                 <ul id="premiumMenu" class="text-center hidden" hidden>
-                    <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="/{{app()->getLocale()}}/cookbook" style="font-size:14px">{{trans('cookbook.title')}}</a></li>
+                    <a class="text-color" href="/{{app()->getLocale()}}/cookbook" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white">{{trans('cookbook.title')}}</li></a>
                 </ul>
                 <ul id="languageMenu" class="text-center hidden" hidden>
-                    <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="/language/set/en-ca" style="font-size:14px"><img class="img img-circle" src="https://cdn.countryflags.com/thumbs/canada/flag-square-500.png" width="15px" /> English (Canada)</a></li>
-                    <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="/language/set/fr-ca" style="font-size:14px"><img class="img img-circle" src="https://cdn.countryflags.com/thumbs/canada/flag-square-500.png" width="15px" /> Français (Canada)</a></li>
+                    <a class="text-color" href="/language/set/en-ca" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><img class="img img-circle" src="https://cdn.countryflags.com/thumbs/canada/flag-square-500.png" width="15px" /> English (Canada)</li></a>
+                    <a class="text-color" href="/language/set/fr-ca" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><img class="img img-circle" src="https://cdn.countryflags.com/thumbs/canada/flag-square-500.png" width="15px" /> Français (Canada)</li></a>
                 </ul>
                 <ul id="profileMenu" class="text-center hidden" hidden>
                     @auth
                         @if(Auth::user()->local_mail)
-                            <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="/{{app()->getLocale()}}/profile/mail" style="font-size:14px"><i class="fa fa-envelope" aria-hidden="true"></i> {{trans('general.mails')}}</a></li>
+                            <a class="text-color" href="/{{app()->getLocale()}}/profile/mail" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><i class="fa fa-envelope" aria-hidden="true"></i> {{trans('general.mails')}}</li></a>
                         @endif
-                        <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="/{{app()->getLocale()}}/profile/edit"  class="{{($page_lists['profile_edit']['inMaintenance']) ? 'hidden' : ''}}" style="font-size:14px"><i class="fa fa-wrench" aria-hidden="true"></i> {{trans('general.edit_profile')}}</a></li>
-                        <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="/logout" style="font-size:14px"><i class="fa fa-sign-out" aria-hidden="true"></i> {{trans('general.logout')}}</a></li>
+                        <a class="text-color" href="/{{app()->getLocale()}}/profile/edit" class="{{($page_lists['profile_edit']['inMaintenance']) ? 'hidden' : ''}}" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><i class="fa fa-wrench" aria-hidden="true"></i> {{trans('general.edit_profile')}}</li></a>
+                        <a class="text-color" href="/logout" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><i class="fa fa-sign-out" aria-hidden="true"></i> {{trans('general.logout')}}</li></a>
                     @endauth
                     @guest
-                        <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="{{ route('login', app()->getLocale()) }}" style="font-size:14px"><i class="fa fa-sign-in" aria-hidden="true"></i> {{trans("general.login")}}</a></li>
-                        <li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><a class="text-color" href="{{ route('register', app()->getLocale()) }}" style="font-size:14px"><i class="fa fa-user-plus" aria-hidden="true"></i> {{trans("general.register")}}</a></li>
+                        <a class="text-color" href="{{ route('login', app()->getLocale()) }}" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><i class="fa fa-sign-in" aria-hidden="true"></i> {{trans("general.login")}}</li></a>
+                        <a class="text-color" href="{{ route('register', app()->getLocale()) }}" style="font-size:14px"><li class="no-decoration" style="padding:5%;margin:2%;border:2px solid white"><i class="fa fa-user-plus" aria-hidden="true"></i> {{trans("general.register")}}</li></a>
                     @endguest
                 </ul>
             </div>
