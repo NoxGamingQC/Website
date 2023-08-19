5645a70461fd3fc6fe0ea9ae7847eb8ec0b517a2
NoxGamingQC <noxgamingqc@gmail.com>
2023-08-19 06:55:05 -0400

    Return 404 for unknown pokemon

--- app/Http/Resources/PokemonResource.php
+++ app/Http/Resources/PokemonResource.php
@@ -15,31 +15,35 @@ class PokemonResource extends JsonResource
     public function toArray($request): array
     {
         $apiUrl = "https://pokeapi.co/api/v2/pokemon/";
-        $pokemonData = json_decode(file_get_contents($apiUrl . $this->resource));
-        $pokemonSpeciesData = json_decode(file_get_contents($pokemonData->species->url));
-        $pokemonAbilities = [];
-        $pokemonTypes = [];
-        foreach($pokemonData->types as $type) {
-            array_push($pokemonTypes, $type->type->name);
+        try {
+            $pokemonData = json_decode(file_get_contents($apiUrl . $this->resource));
+            $pokemonSpeciesData = json_decode(file_get_contents($pokemonData->species->url));
+            $pokemonAbilities = [];
+            $pokemonTypes = [];
+            foreach($pokemonData->types as $type) {
+                array_push($pokemonTypes, $type->type->name);
+            }
+            foreach($pokemonData->abilities as $ability) {
+                array_push($pokemonAbilities, $ability->ability->name);
+            }
+            return [
+                'id' => $pokemonData->id,
+                'name' => $pokemonData->name,
+                'description' => str_replace("\f", " ", str_replace("\n", " ", $pokemonSpeciesData->flavor_text_entries[0]->flavor_text)),
+                'color' => $pokemonSpeciesData->color->name,
+                'height' => $pokemonData->height,
+                'weight' => $pokemonData->weight,
+                'is_legendary' => $pokemonSpeciesData->is_legendary,
+                'is_mythical' => $pokemonSpeciesData->is_mythical,
+                'types' => $pokemonTypes,
+                'abilities' => $pokemonAbilities,
+                'capture_rate' => $pokemonSpeciesData->capture_rate,
+                'growth_rate' => $pokemonSpeciesData->growth_rate->name,
+                'sprite' => $pokemonData->sprites->front_default,
+                'sprite_shiny' => $pokemonData->sprites->front_shiny
+            ];
+        } catch (\Exception $exeption) {
+            abort(404, 'Pokemon not found');
         }
-        foreach($pokemonData->abilities as $ability) {
-            array_push($pokemonAbilities, $ability->ability->name);
-        }
-        return [
-            'id' => $pokemonData->id,
-            'name' => $pokemonData->name,
-            'description' => str_replace("\f", " ", str_replace("\n", " ", $pokemonSpeciesData->flavor_text_entries[0]->flavor_text)),
-            'color' => $pokemonSpeciesData->color->name,
-            'height' => $pokemonData->height,
-            'weight' => $pokemonData->weight,
-            'is_legendary' => $pokemonSpeciesData->is_legendary,
-            'is_mythical' => $pokemonSpeciesData->is_mythical,
-            'types' => $pokemonTypes,
-            'abilities' => $pokemonAbilities,
-            'capture_rate' => $pokemonSpeciesData->capture_rate,
-            'growth_rate' => $pokemonSpeciesData->growth_rate->name,
-            'sprite' => $pokemonData->sprites->front_default,
-            'sprite_shiny' => $pokemonData->sprites->front_shiny
-        ];
     }
 }
