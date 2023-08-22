9cb1be5c878d62679f411b63edbd2967abfc510a
NoxGamingQC <noxgamingqc@gmail.com>
2023-08-22 07:21:14 -0400

    Fix xbox profile api

--- app/Http/Resources/XboxResource.php
+++ app/Http/Resources/XboxResource.php
@@ -22,14 +22,25 @@ class XboxResource extends JsonResource
                 abort(404);
             }
             $preferredColor = json_decode(file_get_contents($result->data->player->meta->preferredColor));
+            $xboxWatermarks = explode('|', $result->data->player->meta->watermarks);
+            $watermarks = [];
+            foreach($xboxWatermarks as $watermark) {
+                if(explode('l', $watermark)[0] == 'ambassador') {
+                    $watermarks[explode('l', $watermark)[0]] = explode('l', $watermark)[1];
+                } else {
+                    if($watermark) {
+                        $watermarks[$watermark] = null;
+                    }
+                }
+            }
             return [
                 'id' => $result->data->player->id,
                 'gamerscore' => $result->data->player->meta->gamerscore,
                 'account_tier' => $result->data->player->meta->accountTier,
                 'xbox_one_rep' => $result->data->player->meta->xboxOneRep,
-                'bio' => $result->data->player->meta->bio,
+                'bio' => $result->data->player->meta->bio ? $result->data->player->meta->bio : null,
                 'tenure_level' => $result->data->player->meta->tenureLevel,
-                'watermarks' => $result->data->player->meta->watermarks,
+                'watermarks' => $watermarks ? $watermarks : null,
                 'username' => $result->data->player->username,
                 'avatar' => $result->data->player->avatar,
                 'preferred_color' => [
@@ -39,7 +50,6 @@ class XboxResource extends JsonResource
                 ],
             ];
         } catch (\Exception $exception) {
-            dd($exception);
             abort(404, 'User not found');
         }
     }
