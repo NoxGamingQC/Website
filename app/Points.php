a2b17db2442179f436af7bbd03e6a9472a005302
NoxGamingQC <noxgamingqc@gmail.com>
2023-08-26 09:08:55 -0400

    Fix point logs

--- app/Points.php
+++ app/Points.php
@@ -9,26 +9,26 @@ class Points extends Model
     protected $table = 'users_points';
 
     static public function getPoints($userID) {
-        $points = User::join('points', 'users.id', '=', 'points.UserID')
-        ->where('points.UserID', '=', $userID)
-        ->orderBy('points.id', 'desc')
-        ->get(['points.UserID', 'users.name', 'points.Quantity', 'points.Comment']);
+        $points = User::join('users_points', 'users.id', '=', 'users_points.user_id')
+        ->where('users_points.user_id', '=', $userID)
+        ->orderBy('users_points.id', 'desc')
+        ->get(['users_points.user_id', 'users.name', 'users_points.quantity', 'users_points.comment']);
         return $points;
     }
 
     static public function getPointsLogs($userID, $quantity = null) {
         if($quantity) {
-            $points = User::join('points', 'users.id', '=', 'points.UserID')
-                ->where('points.UserID', '=', $userID)
-                ->orderBy('points.id', 'desc')
+            $points = User::join('users_points', 'users.id', '=', 'users_points.user_id')
+                ->where('users_points.user_id', '=', $userID)
+                ->orderBy('users_points.id', 'desc')
                 ->take($quantity)
-                ->get(['points.UserID', 'users.name', 'points.Quantity', 'points.Comment']);
+                ->get(['users_points.user_id', 'users.name', 'users_points.quantity', 'users_points.comment']);
             return $points;
         }
-        $points = User::join('points', 'users.id', '=', 'points.UserID')
-                ->where('points.UserID', '=', $userID)
-                ->orderBy('points.id', 'desc')
-                ->get(['points.UserID', 'users.name', 'points.Quantity', 'points.Comment']);
+        $points = User::join('users_points', 'users.id', '=', 'users_points.user_id')
+                ->where('users_points.user_id', '=', $userID)
+                ->orderBy('users_points.id', 'desc')
+                ->get(['users_points.user_id', 'users.name', 'users_points.quantity', 'users_points.comment']);
             return $points;
         
     }
