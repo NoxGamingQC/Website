2dde0925903b1b1e9a326f20ec3000e2e6801f8d
NoxGamingQC <noxgamingqc@gmail.com>
2023-09-04 09:37:01 -0400

    Make theme works

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
@@ -20,19 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       if (env('APP_ENV') == 'production') {
+        if (env('APP_ENV') == 'production') {
             URL::forceScheme('https');
         }
         $mainConfig = MainConfig::all();
         foreach($mainConfig as $key => $value) {
-            if($value->slug == 'theme') {
-                $themeName = $value->value;
-            }
             if($value->slug == 'maintenance') {
                 $maintenance = $value->value;
-            }
-            if($value->slug == 'force_theme') {
-                $forceTheme = $value->value;
             }
             if($value->slug == 'headline_hr') {
                 $headlineHr = $value->value;
@@ -61,82 +56,74 @@ class AppServiceProvider extends ServiceProvider
             $sourceVersion = 'undefined';
         }
 
-
-        if(!$forceTheme) {
-            $officialtheme = Theme::where('name', '=', $themeName)->first();
-            if($officialtheme) {
-                $theme =  collect([
-                    'name' => $themeName,
-                    'force' => $forceTheme,
-                    'custom' => Auth::check() ? (count(CustomTheme::where('owner_id', '=', Auth::user()->id)->first()) ? true : false) : false,
-                    'primary_text' => (hexdec(substr($officialtheme->primary, 0, 2)) * 0.266 + hexdec(substr($officialtheme->primary, 2, 2)) * 0.587 + hexdec(substr($officialtheme->primary, 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
-                    'background_text' => (hexdec(substr($officialtheme->background, 0, 2)) * 0.266 + hexdec(substr($officialtheme->background, 2, 2)) * 0.587 + hexdec(substr($officialtheme->background, 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
-                    'background' => $officialtheme->background,
-                    'primary' => $officialtheme->primary,
-                    'blue' => $officialtheme->blue,
-                    'green' => $officialtheme->green,
-                    'yellow' => $officialtheme->yellow,
-                    'red' => $officialtheme->red,
-                    'gray' => $officialtheme->gray
-                ]);
+        view()->composer('*', function ($view) {
+            $mainConfig = MainConfig::all();
+            foreach($mainConfig as $key => $value) {
+                if($value->slug == 'theme') {
+                    $themeName = $value->value;
+                }
+                if($value->slug == 'force_theme') {
+                    $forceTheme = $value->value;
+                }
             }
-        } else {
-            if(Auth::check()) {
-                if(count(CustomTheme::where('owner_id', '=', Auth::user()->id)->first())) {
-                    $customTheme = CustomTheme::where('owner_id', '=', Auth::user()->id)->first();
+            if($forceTheme === 'false') {
+                if(auth()->check()) {
+                    if(auth()->user()->theme === 'custom') {
+                        if(count(CustomTheme::where('owner_id', '=', auth()->user()->id)->first())) {
+                            $customTheme = CustomTheme::where('owner_id', '=', auth()->user()->id)->first();
+                            $theme =  collect([
+                                'name' => 'custom',
+                                'background' => $customTheme->background,
+                                'primary' => $customTheme->primary,
+                                'primary_text' => (hexdec(substr($customTheme->primary, 0, 2)) * 0.266 + hexdec(substr($customTheme->primary, 2, 2)) * 0.587 + hexdec(substr($customTheme->primary, 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
+                                'background_text' => (hexdec(substr($customTheme->background, 0, 2)) * 0.266 + hexdec(substr($customTheme->background, 2, 2)) * 0.587 + hexdec(substr($customTheme->background, 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
+                                'blue' => $customTheme->blue,
+                                'green' => $customTheme->green,
+                                'yellow' => $customTheme->yellow,
+                                'red' => $customTheme->red,
+                                'gray' => $customTheme->gray
+                            ]);
+                        }
+                    } else {
+                        $officialtheme = Theme::where('name', '=', auth()->user()->theme)->first();
+                        if($officialtheme) {
+                            $theme =  collect([
+                                'name' => $officialtheme->name,
+                                'primary_text' => (hexdec(substr($officialtheme->primary, 0, 2)) * 0.266 + hexdec(substr($officialtheme->primary, 2, 2)) * 0.587 + hexdec(substr($officialtheme->primary, 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
+                                'background_text' => (hexdec(substr($officialtheme->background, 0, 2)) * 0.266 + hexdec(substr($officialtheme->background, 2, 2)) * 0.587 + hexdec(substr($officialtheme->background, 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
+                                'background' => $officialtheme->background,
+                                'primary' => $officialtheme->primary,
+                                'blue' => $officialtheme->blue,
+                                'green' => $officialtheme->green,
+                                'yellow' => $officialtheme->yellow,
+                                'red' => $officialtheme->red,
+                                'gray' => $officialtheme->gray
+                            ]);
+                        }
+                    }
+                }
+            }
+            if(!isset($theme)) {
+                $officialtheme = Theme::where('name', '=', $themeName)->first();
+                if($officialtheme) {
                     $theme =  collect([
-                        'name' => 'custom',
-                        'force' => false,
-                        'background' => $customTheme->background,
-                        'primary' => $customTheme->primary,
-                        'primary_text' => (hexdec(substr($customTheme->primary, 0, 2)) * 0.266 + hexdec(substr($customTheme->primary, 2, 2)) * 0.587 + hexdec(substr($customTheme->primary, 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
-                        'background_text' => (hexdec(substr($customTheme->background, 0, 2)) * 0.266 + hexdec(substr($customTheme->background, 2, 2)) * 0.587 + hexdec(substr($customTheme->background, 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
-                        'blue' => $customTheme->blue,
-                        'green' => $customTheme->green,
-                        'yellow' => $customTheme->yellow,
-                        'red' => $customTheme->red,
-                        'gray' => $customTheme->gray
+                        'name' => $themeName,
+                        'background' => $officialtheme->background,
+                        'primary' => $officialtheme->primary,
+                        'primary_text' => (hexdec(substr($officialtheme->primary, 0, 2)) * 0.266 + hexdec(substr($officialtheme->primary, 2, 2)) * 0.587 + hexdec(substr($officialtheme->primary, 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
+                        'background_text' => (hexdec(substr($officialtheme->background, 0, 2)) * 0.266 + hexdec(substr($officialtheme->background, 2, 2)) * 0.587 + hexdec(substr($officialtheme->background, 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
+                        'blue' => $officialtheme->blue,
+                        'green' => $officialtheme->green,
+                        'yellow' => $officialtheme->yellow,
+                        'red' => $officialtheme->red,
+                        'gray' => $officialtheme->gray
                     ]);
                 }
             }
-        }
-        if(!isset($theme)) {
-            $officialtheme = Theme::where('name', '=', $themeName)->first();
-            if($officialtheme) {
-                $theme =  collect([
-                    'name' => $themeName,
-                    'force' => $forceTheme,
-                    'custom' => Auth::check() ? (count(CustomTheme::where('owner_id', '=', Auth::user()->id)->first()) ? true : false) : false,
-                    'background' => $officialtheme->background,
-                    'primary' => $officialtheme->primary,
-                    'primary_text' => (hexdec(substr($officialtheme->primary, 0, 2)) * 0.266 + hexdec(substr($officialtheme->primary, 2, 2)) * 0.587 + hexdec(substr($officialtheme->primary, 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
-                    'background_text' => (hexdec(substr($officialtheme->background, 0, 2)) * 0.266 + hexdec(substr($officialtheme->background, 2, 2)) * 0.587 + hexdec(substr($officialtheme->background, 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
-                    'blue' => $officialtheme->blue,
-                    'green' => $officialtheme->green,
-                    'yellow' => $officialtheme->yellow,
-                    'red' => $officialtheme->red,
-                    'gray' => $officialtheme->gray
-                ]);
-            }
-        }
-        
-        /*$theme = collect([
-            'name' => $themeName,
-            'force' => $forceTheme,
-            'custom' => Auth::check() ? (count(CustomTheme::where('owner_id', '=', Auth::user()->id)->first()) ? true : false) : false,
-            'background' => '555555',
-            'primary' => 'ff8800',
-            'primary_text' => (hexdec(substr('ff8800', 0, 2)) * 0.266 + hexdec(substr('ff8800', 2, 2)) * 0.587 + hexdec(substr('ff8800', 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
-            'background_text' => (hexdec(substr('555555', 0, 2)) * 0.266 + hexdec(substr('555555', 2, 2)) * 0.587 + hexdec(substr('555555', 4, 2)) * 0.114) > 186 ? '000000' : 'FFFFFF',
-            'blue' => '0000FF',
-            'green' => '00FF00',
-            'yellow' => 'FFFF00',
-            'red' => 'FFFF00',
-            'gray' => 'aaaaaa'
-        ]);*/
+            $view->with('theme', json_decode($theme->toJson()));
+        });
 
         return view()->share([
-            'theme' => json_decode($theme->toJson()),
             'headline' => $headline,
             'sourceVersion' => $sourceVersion
         ]);
--- resources/views/layouts/style.blade.php
+++ resources/views/layouts/style.blade.php
@@ -15,13 +15,21 @@
         color: #{{$theme->primary}} !important;
     }
     .btn-primary {
-        background-color: #{{$theme->primary}};
-        color: #{{$theme->primary_text}}
+        background-color: #{{((strlen(dechex(hexdec(substr($theme->background, 0, 2)) * 0.75)) == 1) ? ('0' . dechex(hexdec(substr($theme->background, 0, 2)) * 0.75)) : (dechex(hexdec(substr($theme->background, 0, 2)) * 0.75))) . ((strlen(dechex(hexdec(substr($theme->background, 2, 2)) * 0.75)) == 1) ? ('0' . dechex(hexdec(substr($theme->background, 2, 2)) * 0.75)) : (dechex(hexdec(substr($theme->background, 2, 2)) * 0.75))) .  ((strlen(dechex(hexdec(substr($theme->background, 4, 2)) * 0.75)) == 1) ? ('0' . dechex(hexdec(substr($theme->background, 4, 2)) * 0.75)) : (dechex(hexdec(substr($theme->background, 4, 2)) * 0.75)))}};
+        color: #{{$theme->primary_text}};
+        border-color: #{{((strlen(dechex(hexdec(substr($theme->background, 0, 2)) * 0.75)) == 1) ? ('0' . dechex(hexdec(substr($theme->background, 0, 2)) * 0.75)) : (dechex(hexdec(substr($theme->background, 0, 2)) * 0.75))) . ((strlen(dechex(hexdec(substr($theme->background, 2, 2)) * 0.75)) == 1) ? ('0' . dechex(hexdec(substr($theme->background, 2, 2)) * 0.75)) : (dechex(hexdec(substr($theme->background, 2, 2)) * 0.75))) .  ((strlen(dechex(hexdec(substr($theme->background, 4, 2)) * 0.75)) == 1) ? ('0' . dechex(hexdec(substr($theme->background, 4, 2)) * 0.75)) : (dechex(hexdec(substr($theme->background, 4, 2)) * 0.75)))}} !important;
+        padding: 15px;
+        border-radius: 15px;
     }
     .btn-primary:hover, .btn-primary:active {
-        background-color: #{{$theme->primary}};
+        background-color: #{{$theme->background}};
+        border-color: #{{ $theme->primary }} !important;
         color: #{{$theme->primary_text}};
-        filter: brightness(0.85);
+        box-shadow: 0 0 .1rem #{{((strlen(dechex(hexdec(substr($theme->primary, 0, 2)) * 1.5)) == 1) ? ('0' . dechex(hexdec(substr($theme->primary, 0, 2)) * 1.5)) : (dechex(hexdec(substr($theme->primary, 0, 2)) * 1.5))) . ((strlen(dechex(hexdec(substr($theme->primary, 2, 2)) * 1.5)) == 1) ? ('0' . dechex(hexdec(substr($theme->primary, 2, 2)) * 1.5)) : (dechex(hexdec(substr($theme->primary, 2, 2)) * 1.5))) .  ((strlen(dechex(hexdec(substr($theme->primary, 4, 2)) * 1.5)) == 1) ? ('0' . dechex(hexdec(substr($theme->primary, 4, 2)) * 1.5)) : (dechex(hexdec(substr($theme->primary, 4, 2)) * 1.5)))}},
+            0 0 .1rem #{{((strlen(dechex(hexdec(substr($theme->primary, 0, 2)) * 1.5)) == 1) ? ('0' . dechex(hexdec(substr($theme->primary, 0, 2)) * 1.5)) : (dechex(hexdec(substr($theme->primary, 0, 2)) * 1.5))) . ((strlen(dechex(hexdec(substr($theme->primary, 2, 2)) * 1.5)) == 1) ? ('0' . dechex(hexdec(substr($theme->primary, 2, 2)) * 1.5)) : (dechex(hexdec(substr($theme->primary, 2, 2)) * 1.5))) .  ((strlen(dechex(hexdec(substr($theme->primary, 4, 2)) * 1.5)) == 1) ? ('0' . dechex(hexdec(substr($theme->primary, 4, 2)) * 1.5)) : (dechex(hexdec(substr($theme->primary, 4, 2)) * 1.5)))}},
+            0 0 1rem #{{ $theme->primary }},
+            0 0 0.1rem #{{ $theme->primary }},
+            inset 0 0 1.1rem #{{ $theme->primary }}; 
     }
     .panel-primary {
         background-color: #{{$theme->background}};
@@ -37,5 +45,9 @@
         border: 1px solid #{{$theme->primary}};
         border-color: #{{$theme->primary}} !important;
         border-radius: 25px;
+    }
+
+    .social-bar-item {
+        padding: 12px 11px !important;
     }
 </style>
