485f4e139b3d5cf14fcd9aa23c317a844f96e67f
NoxGamingQC <noxgamingqc@gmail.com>
2023-09-27 12:56:02 -0400

    Fix issue where cant open localhost

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') == 'production') {
+        if (env('APP_URL') !== 'http://localhost:8000') {
             URL::forceScheme('https');
         }
         $mainConfig = MainConfig::all();
