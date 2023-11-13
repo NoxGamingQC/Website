e529599b4de50b2cb4f88e5be80f588cb75116d2
NoxGamingQC <noxgamingqc@gmail.com>
2023-11-13 07:57:23 -0500

    Fix error page

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,16 +1,15 @@
-@extends('layouts.noxgamingqc.app')
-@section('title', 'ERROR 401')
-@section('slogan', 'Woops, you are not suppose to be here. OwO')
+@extends('layouts.error')
+@section('title', 'ERROR 503')
 @section('content')
 
-
 <div class="row">
-    <div class="col-md-12 bg-dark content-item">
-        <div class="container">
-            <h3 class="raleway-font text-danger">ERROR 401 - UNAUTHORIZED ACCESS</h3>
-            <p class="raleway-font text-danger">Something wrong happen.</p>
+        <div class="container text-center">
+            <img src="/img/logo.svg" width="300">
+            <h3 class="raleway-font">ERROR 401 | Access not authorised.</h3>
+            <br />
+            <hr />
+            <br />
+            <h3 class="raleway-font">ERREUR 401 | Accès non authorisé.</h3>
         </div>
     </div>
 </div>
-
-@endsection
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,14 +1,15 @@
-@extends('layouts.noxgamingqc.app')
+@extends('layouts.error')
 @section('title', 'ERROR 503')
-@section('slogan', 'Under maintenance. Sorry for any inconveniance.')
 @section('content')
 
-
 <div class="row">
-    <div class="col-md-12 bg-dark content-item">
-        <div class="container">
-            <h3 class="raleway-font text-danger">ERROR 503 - Service Unavailable.</h3>
-            <p class="raleway-font text-danger">This page/section is under maintenance. Sorry for any inconveniance. It should be back in a couple of days. Try again later.</p>
+        <div class="container text-center">
+            <img src="/img/logo.svg" width="300">
+            <h3 class="raleway-font">ERROR 503 | In maintenance.</h3>
+            <br />
+            <hr />
+            <br />
+            <h3 class="raleway-font">ERREUR 503 | En maintenance.</h3>
         </div>
     </div>
 </div>
