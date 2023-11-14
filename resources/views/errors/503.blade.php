36595801dd276ba27289d5b1f29c09d5b9c9f499
NoxGamingQC <noxgamingqc@gmail.com>
2023-11-14 12:40:45 -0500

    Change screen for maintenance

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.error')
+@extends('layouts.maintenance')
 @section('title', 'ERROR 503')
 @section('content')
 
-<div class="row {{app('request')->input('app_type') !== 'mobile_app' ? 'error-background' : ''}}">
+<div class="row">
         <div class="container text-center">
             <img src="/img/logo.svg" width="300">
             <h3 class="raleway-font">ERROR 503 | In maintenance.</h3>
