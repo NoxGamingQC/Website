aa919a0906fb2ea6ee80fa85472c28be4186afd8
NoxGamingQC <noxgamingqc@gmail.com>
2023-11-14 20:39:05 -0500

    Fix social button on welcome page

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.error')
+@extends('layouts.pages.error')
 @section('title', 'ERROR 503')
 @section('content')
 
--- resources/views/layouts/components/socials.blade.php
+++ resources/views/layouts/components/socials.blade.php
@@ -1,12 +1,12 @@
-<a class="btn btn-twitch social-bar-item btn-circle" href="https://www.twitch.tv/noxgamingqc">
+<a class="btn btn-twitch social-bar-item btn-circle btn-small" href="https://www.twitch.tv/noxgamingqc">
     <i class="fa fa-twitch" aria-hidden="true"></i>
 </a>
-<a class="btn btn-youtube social-bar-item btn-circle" href="https://www.youtube.com/channel/UCytKDUapog2tnJD4XenehiQ">
+<a class="btn btn-youtube social-bar-item btn-circle btn-small" href="https://www.youtube.com/channel/UCytKDUapog2tnJD4XenehiQ">
     <i class="fa fa-youtube-play" aria-hidden="true"></i>
 </a>
-<a class="btn btn-twitter social-bar-item btn-circle" href="https://www.twitter.com/noxgamingqc">
+<a class="btn btn-twitter social-bar-item btn-circle btn-small" href="https://www.twitter.com/noxgamingqc">
     <i class="fa fa-twitter" aria-hidden="true"></i>
 </a>
-<a class="btn btn-instagram social-bar-item btn-circle" href="https://www.instagram.com/noxgamingqc">
+<a class="btn btn-instagram social-bar-item btn-circle btn-small" href="https://www.instagram.com/noxgamingqc">
     <i class="fa fa-instagram" aria-hidden="true"></i>
 </a>
