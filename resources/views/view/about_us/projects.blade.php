53d8d0b922aa3b17675f43016f1bfe8d503b293d
NoxGamingQC <noxgamingqc@gmail.com>
2023-11-16 07:46:14 -0500

    Fix project page

--- resources/views/view/about_us/projects.blade.php
+++ resources/views/view/about_us/projects.blade.php
@@ -7,12 +7,14 @@
     <div class="row">
         <div class="col-md-12 content-item">
             <div class="col-md-6 text-left">
+                <br />
+                <br />
                 <h4>Our website</h4>
                 <br />
                 <p>{{trans('projects.website_description')}} <a class="btn btn-primary" href="https://www.noxgamingqc.ca">Visit</a></p>
-                
             </div>
             <div class="col-md-6">
+                <img class="text-center" src="/img/no-image.png" width="250px" />
             </div>
         </div>
     </div>
@@ -21,28 +23,17 @@
     <div class="row">
         <div class="col-md-12 bg-dark content-item">
             <div class="col-md-6">
+                <img class="text-center" src="/img/no-image.png" width="250px" />
             </div>
             <div class="col-md-6 text-right">
+                <br />
+                <br />
+                <br />
                 <h4>NoxBOT</h4>
                 <br />
                 <p><a class="btn btn-primary" href="https://github.noxgamingqc.ca/NoxBOT">Visit</a>  {{trans('projects.noxbot_description')}}</p>
-                
             </div>
         </div>
     </div>
 </div>
-<div class="container section">
-    <div class="row">
-        <div class="col-md-12 bg-dark content-item">
-            <div class="col-md-6 text-center">
-                <img src="/img/Projects/panacea-thumbnail.png" height="175px" />
-            </div>
-            <div class="col-md-6 text-right">
-                <h4>Project Panacea</h4>
-                <br />
-                <p>In development <a class="btn btn-primary disables" disabled>soon</a></p>
-            </div>
-        </div>
-    </div>
-</div>
-@stop
+@endsection
