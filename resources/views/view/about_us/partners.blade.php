449ec8834a7856a69a8ef1083e70d9175cfe85de
NoxGamingQC <noxgamingqc@gmail.com>
2023-11-15 23:01:45 -0500

    Fix partners and teams page

--- resources/views/view/about_us/partners.blade.php
+++ resources/views/view/about_us/partners.blade.php
@@ -13,15 +13,15 @@
                 <div class="col-md-12">
                     <div class="row">
                         <div class="col-md-offset-1 col-md-5">
-                            <div class="panel panel-primary text-center">
-                                <div class="panel-body img" style="background-image: url('/img/Partners/CorpsRoyaux.png') !important; background-size: cover !important; height: 200px !important;background-position:center center">
+                            <div class="panel panel-default text-center">
+                                <div class="panel-body" style="background-image: url('/img/Partners/CorpsRoyaux.png') !important; background-size: cover !important; height: 200px !important;background-position:center center">
                                     <h3 class="text-badge"><b>Corps Royaux</b></h3>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-5">
-                            <div class="panel panel-primary text-center">
-                                <div class="panel-body img" style="background-image: url('/img/Partners/Instant-Gaming.png') !important; background-size: cover !important; height: 200px !important;background-position:center center">
+                            <div class="panel panel-default text-center">
+                                <div class="panel-body" style="background-image: url('/img/Partners/Instant-Gaming.png') !important; background-size: cover !important; height: 200px !important;background-position:center center">
                                     <h3 class="text-badge"><b>Instant Gaming</b></h3>
                                 </div>
                             </div>
@@ -33,7 +33,7 @@
     </div>
 </div>
 <div class="container section">
-    <div class="col-md-12 item-content bg-dark">
+    <div class="col-md-12">
         <div class="row">
             <div class="col-md-12" id="title">
                 <h2>{{trans('general.affiliates')}}</h2>
@@ -42,14 +42,14 @@
             <div class="col-md-12">
                 <div class="row">
                     <div class="col-md-5 col-md-offset-1">
-                        <div class="panel panel-primary text-center">
+                        <div class="panel panel-default text-center">
                             <div class="panel-body extra-life-logo" style="height: 200px !important; background-position:center center">
                                 <h3 class="text-badge"><b>Extra Life</b></h3>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-5">
-                        <div class="panel panel-primary text-center">
+                        <div class="panel panel-default text-center">
                             <div class="panel-body img" style="background-image: url('/img/Affiliates/HumbleBundle.png') !important; background-size: cover !important; height: 200px !important; background-position:center center">
                                 <h3 class="text-badge"><b>Humble Bundle</b></h3>
                             </div>
