1ac226f92dc059cfc350be395081d11f832c56a8
NoxGamingQC <noxgamingqc@gmail.com>
2023-11-15 23:01:45 -0500

    Fix partners and teams page

--- resources/views/view/about_us/teams.blade.php
+++ resources/views/view/about_us/teams.blade.php
@@ -8,7 +8,7 @@
         <div class="col-md-12 content-item">
             <div class="row">
             <div class="col-md-4">
-                    <div class="panel panel-primary text-center">
+                    <div class="panel panel-default text-center">
                         <div class="panel-body img" style="background-image: url('/img/Teams/PositivityST.jpg') !important; background-size: cover !important; height: 200px !important; background-position:center center">
                             <h2 class="text-badge"><b>Positivity Stream Team</b></h2>
                             <p class="text-badge"><b>Manager</b></p>
@@ -16,7 +16,7 @@
                     </div>
                 </div>    
                 <div class="col-md-4">
-                    <div class="panel panel-primary text-center">
+                    <div class="panel panel-default text-center">
                         <div class="panel-body img" style="background-image: url('/img/Teams/LaDreamTeam.png') !important; background-size: cover !important; height: 200px !important; background-position:center center">
                             <h2 class="text-badge"><b>La Dream Team</b></h2>
                             <p class="text-badge"><b>Moderator</b></p>
@@ -24,7 +24,7 @@
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="panel panel-primary text-center">
+                    <div class="panel panel-default text-center">
                         <div class="panel-body img" style="background-image: url('/img/Teams/LZB Racing.png') !important; background-size: cover !important; height: 200px !important; background-position:center center">
                             <h2 class="text-badge"><b>Lay-Z-Boy Racing</b></h2>
                             <p class="text-badge"><b>Member</b></p>
@@ -40,7 +40,7 @@
         <div class="col-md-12 bg-dark content-item">
             <div class="row">
                 <div class="col-md-4 col-md-offset-4">
-                    <div class="panel panel-primary text-center">
+                    <div class="panel panel-default text-center">
                         <div class="panel-body  background-position:center centerimg" style="background-image: url('/img/Teams/Irae Gaming.png') !important; background-size: cover !important; height: 200px !important; background-position:center center">
                             <h2 class="text-badge"><b>Irae Gaming</b></h2>
                             <p class="text-badge"><b>Member</b></p>
