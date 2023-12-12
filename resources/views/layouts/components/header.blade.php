868c0c619cc93169ed4972d61c166451ffbbad5c
NoxGamingQC <noxgamingqc@gmail.com>
2023-12-12 18:08:30 -0500

    Add store link

---
+++ resources/views/layouts/components/header.blade.php
@@ -0,0 +1,8 @@
+<div class="header" style="background-color:#{{$theme->primary}};background: linear-gradient(220deg, #{{$theme->primary}}, #{{$theme->background}});">
+    <div class="row text-center">
+        <h1 class="text-highlight headline">@yield('title')</h1>
+        <br />
+        <h3 class="raleway-font text-highlight headline" style="margin-left:5%;max-width:90%">@yield('slogan')</h3>
+        <br />
+    </div>
+</div>
