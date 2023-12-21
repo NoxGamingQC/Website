ebbb871e2776c8b453409a5da04f27fb826f0619
NoxGamingQC <noxgamingqc@gmail.com>
2023-12-20 22:37:43 -0500

    Fix error page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container">
     <div class="row text-center">
-        <img src="/img/logo.svg" width="300">
+        <img src="/img/logo.png" width="300">
         <h3 class="raleway-font">ERROR 503 | In maintenance.</h3>
         <br />
         <hr />
