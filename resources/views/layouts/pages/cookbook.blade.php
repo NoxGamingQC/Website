7a85eb3fde46fb64435501789fc5d23f54ea9aa6
NoxGamingQC <noxgamingqc@gmail.com>
2023-12-24 15:12:42 -0500

    Hide cookbook screen if too long

--- resources/views/layouts/pages/cookbook.blade.php
+++ resources/views/layouts/pages/cookbook.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}" style="background-color:#{{$theme->background}}">
+<html lang="{{ app()->getLocale() }}" style="background-color:#{{$theme->background}};">
     <head>
         <meta name="google-site-verification" content="D30gPHSCahf2lVeDo0Ndgc8vI1cQvv8d1gXIZa3B2ds" />
         <meta name="facebook-domain-verification" content="uki484ngemqhks0g9endzi9hb1nobp" />
@@ -26,7 +26,9 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
         <script src="{{mix('js/app.js')}}"></script>
     </head>
-    <body style="background-color:#{{$theme->background}}">
+    <body style="background-color:#{{$theme->background}};">
+        <div class="idle-screen" style="position:absolute; width:100%;height:100%;top:0px;bottom:0px;background-color:#000;z-index:99;overflow:hidden" hidden></div>
+        <div class="active-screen">
         <input id="websiteLocationID" type="hidden">
         @include('alert.alert')
         @include('layouts.components.cookbook.header')
@@ -42,6 +44,29 @@
             console.log('%c{!!trans('general.console_copy_paste01')!!}', 'color:#FFF; font-size:18px;');
             console.log('%c{{trans('general.console_copy_paste02')}}', 'color:#F00; font-size:18px;');
             console.log('%c{{trans('general.console_close_window')}}', 'color:#FFF; font-size:18px;');
+
+            var idleTime = 0;
+            $(document).ready(function () {
+                var idleInterval = setInterval(timerIncrement, 60000);
+                $(this).mousemove(function (e) {
+                    idleTime = 0;
+                    $('.idle-screen').addClass('hidden');
+                    $('.active-screen').removeClass('hidden');
+                });
+                $(this).keypress(function (e) {
+                    idleTime = 0;
+                    $('.idle-screen').addClass('hidden');
+                    $('.active-screen').removeClass('hidden');
+                });
+            });
+
+            function timerIncrement() {
+                idleTime = idleTime + 1;
+                if (idleTime > 59) {
+                    $('.idle-screen').removeClass('hidden');
+                    $('.active-screen').addClass('hidden');
+                }
+            }
         </script>
     </body>
 </html>
