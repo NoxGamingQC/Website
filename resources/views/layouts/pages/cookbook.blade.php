3995d768b0653fc25af94bad144ef6e25b3fe184
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2023-12-25 14:52:24 -0500

    Update cookbook.blade.php

--- resources/views/layouts/pages/cookbook.blade.php
+++ resources/views/layouts/pages/cookbook.blade.php
@@ -21,7 +21,7 @@
         <title>{{env('APP_NAME')}} - @yield('title')</title>
         @endif
         <link rel="icon" href="/img/logo.png" type="image/png">
-        <link href="{{mix('css/system.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{mix('css/dark.css')}}" rel="stylesheet" type="text/css">
         <link href="{{mix('css/bootstrap.css')}}" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
         <script src="{{mix('js/app.js')}}"></script>
