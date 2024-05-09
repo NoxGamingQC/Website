cdaa90b1edd1102698d1eed1eecc072ef75c82ce
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2024-05-08 20:12:00 -0400

    Change modules list view

--- resources/views/view/management/modules.blade.php
+++ resources/views/view/management/modules.blade.php
@@ -2,30 +2,36 @@
 @section('title', 'Management - Modules')
 @section('content')
 
-<div class="row bg-dark" style="padding-top:10%;padding-bottom:10%">
-    <div class="col-md-12">
+<div class="row">
+    <div class="col-md-12 content-item">
         <div class="container-fluid">
-            @foreach($modules as $key => $module)
-                <div class="col-md-4">
-                    <div class="text-center">
-                        <h4><b>{{ $module['icon'] }} {{$module['slug']}}</b></h4>
-                        <br />
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="col-md-2">
-                                <label>Module Status</label>
-                            </div>
-                            <div class="col-md-1">
+            <h1>Modules list</h1>
+            <div class="col-md-12 table-responsive" style="padding-top:3%; padding-bottom:5%">
+                <table class="table" style="border: 1px solid #CCC">
+                    <thead>
+                        <tr>
+                            <th scope="col" class="col-md-1"><p>Icon</p></th>
+                            <th scope="col" class="col-md-1"><p>Name</p></th>
+                            <th scope="col" class="col-md-3"><p>Status</p></th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($modules as $key => $module)
+                        <tr>
+                            <td><p class=""> {{ $module['icon'] }}</p></td>
+                            <td><p class=""> {{ $module['slug'] }}</p></td>
+                            <td>
                                 <select class="selectpicker">
                                     <option {{ !$module['isInMaintenance'] ? 'selected' : '' }}>Active</option>
                                     <option {{ $module['isInMaintenance'] ? 'selected' : '' }}>Disable (In maintenance)</option>
                                 </select>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
