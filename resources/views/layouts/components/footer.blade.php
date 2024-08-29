80cd6aae20a6a55f64ebbb323c5741ec219f06ac
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2024-08-29 09:57:09 -0400

    Add color to footer ps error page looks weird now

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -1,29 +1,31 @@
-<div class="container">
-    <div class="row footer">
-    <div class="col-md-4 text-center">
-            <h3>
-                Contact
-            </h3>
-            <br />
-            <h5 class="raleway-font">jbedard@noxgamingqc.ca</h5>
-        </div>
+<div class="footer">
+    <div class="container">
+        <div class="row">
         <div class="col-md-4 text-center">
-            <img src="/img/logo.png" alt="logo" width="150px">
-            <br />
-            <h5 class="raleway-font">{{trans('general.copyright_noxgamingqc')}}</h5>
-            <br />
-        </div>
-        <div class="col-md-4 text-center">
-            <h3>
-                Socials
-            </h3>
-            <br />
-            <p>
-                @include('layouts.components.socials')
-            </p>
-        </div>
-        <div class="col-md-12 text-center">
-            <p><small>commit: {{$sourceVersion}}<small></p>
+                <h3>
+                    Contact
+                </h3>
+                <br />
+                <h5 class="raleway-font">jbedard@noxgamingqc.ca</h5>
+            </div>
+            <div class="col-md-4 text-center">
+                <img src="/img/logo.png" alt="logo" width="150px">
+                <br />
+                <h5 class="raleway-font">{{trans('general.copyright_noxgamingqc')}}</h5>
+                <br />
+            </div>
+            <div class="col-md-4 text-center">
+                <h3>
+                    Socials
+                </h3>
+                <br />
+                <p>
+                    @include('layouts.components.socials')
+                </p>
+            </div>
+            <div class="col-md-12 text-center">
+                <p><small>commit: {{$sourceVersion}}<small></p>
+            </div>
         </div>
     </div>
 </div>
