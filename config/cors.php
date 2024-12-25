da30780134b5e99b4bbfd6dc5b1c9b93db23ab7b
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2024-12-24 21:46:03 -0500

    Disabling cors

--- config/cors.php
+++ config/cors.php
@@ -11,20 +11,14 @@ return [
     | to accept any value.
     |
     */
-    'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
 
-    // explicitly tell which origins needs access to the resource
-    'allowed_origins' => ['*'],
-
-    // or use regex pattern, helpful if you want to grant
-    // access to origins with certain pattern (i.e. an origin under a subdomain etc.)
-    'allowed_origins_patterns' => ['*'],
-
-    // no changes made below
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => false,
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['*'],
+    'allowedOriginsPatterns' => [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'paths' => ['*'],
+    'maxAge' => 0,
 
 ];
