2c8719e6cdcd31188d109152736a31f8a2d7113c
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2025-01-30 09:28:41 -0500

    Fix panels

--- resources/views/alert/alert.blade.php
+++ resources/views/alert/alert.blade.php
@@ -1,91 +1,75 @@
 
-@if (Session::has('success'))
-    <div class="row alert alert-dismissible" role="alert">
-        <div class="col-md-12">
-            <div class="panel panel-success">
-                <div class="panel-body">
-                    <div class="col-md-9">
-                        <strong>Success! </strong> {{ session('success') }}
-                    </div>
-                    <div class="col-md-3">
-                        <button type="button" class="close" data-dismiss="alert">
-                            <i class="fa fa-times"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endif
-@if (Session::has('error'))
-    <div class="row alert alert-dismissible" role="alert">
-        <div class="col-md-12">
-            <div class="panel panel-danger">
-                <div class="panel-body">
-                    <div class="col-md-9">
-                        <strong>Error! </strong> {{ session('error') }}
-                    </div>
-                    <div class="col-md-3">
-                        <button type="button" class="close" data-dismiss="alert">
-                            <i class="fa fa-times"></i>
-                        </button>
+<div class="col-md-12" style="padding-top:8%">
+    @if (Session::has('success'))
+        <div class="row alert alert-dismissible" role="alert">
+            <div class="col-md-12">
+                <div class="panel panel-success">
+                    <div class="panel-body">
+                        <div class="col-md-9">
+                            <strong>Success! </strong> {{ session('success') }}
+                        </div>
+                        <div class="col-md-3">
+                            <button type="button" class="close" data-dismiss="alert">
+                                <i class="fa fa-times"></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-@endif
-@if (Session::has('warning'))
-    <div class="row alert alert-dismissible" role="alert">
-        <div class="col-md-12">
-            <div class="panel panel-warning">
-                <div class="panel-body">
-                    <div class="col-md-9">
-                        <strong>Success! </strong> {{ session('warning') }}
-                    </div>
-                    <div class="col-md-3">
-                        <button type="button" class="close" data-dismiss="alert">
-                            <i class="fa fa-times"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endif
-@if (Session::has('info'))
-    <div class="row alert alert-dismissible" role="alert">
-        <div class="col-md-12">
-            <div class="panel panel-info">
-                <div class="panel-body">
-                    <div class="col-md-9">
-                        <strong>Success! </strong> {{ session('info') }}
-                    </div>
-                    <div class="col-md-3">
-                        <button type="button" class="close" data-dismiss="alert">
-                            <i class="fa fa-times"></i>
-                        </button>
+    @endif
+    @if (Session::has('error'))
+        <div class="row alert alert-dismissible" role="alert">
+            <div class="col-md-12">
+                <div class="panel panel-danger">
+                    <div class="panel-body">
+                        <div class="col-md-9">
+                            <strong>Error! </strong> {{ session('error') }}
+                        </div>
+                        <div class="col-md-3">
+                            <button type="button" class="close" data-dismiss="alert">
+                                <i class="fa fa-times"></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-@endif
-@if (Session::has('primary'))
-    <div class="row alert alert-dismissible" role="alert">
-        <div class="col-md-12">
-            <div class="panel panel-primary">
-                <div class="panel-body">
-                    <div class="col-md-9">
-                        <strong>Success! </strong> {{ session('primary') }}
-                    </div>
-                    <div class="col-md-3">
-                        <button type="button" class="close" data-dismiss="alert">
-                            <i class="fa fa-times"></i>
-                        </button>
+    @endif
+    @if (Session::has('warning'))
+        <div class="row alert alert-dismissible" role="alert">
+            <div class="col-md-12">
+                <div class="panel panel-warning">
+                    <div class="panel-body">
+                        <div class="col-md-9">
+                            <strong>Success! </strong> {{ session('warning') }}
+                        </div>
+                        <div class="col-md-3">
+                            <button type="button" class="close" data-dismiss="alert">
+                                <i class="fa fa-times"></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-@endif
+    @endif
+    @if (Session::has('info'))
+        <div class="row alert alert-dismissible" role="alert">
+            <div class="col-md-12">
+                <div class="panel panel-info">
+                    <div class="panel-body">
+                        <div class="col-md-9">
+                            <strong>Success! </strong> {{ session('info') }}
+                        </div>
+                        <div class="col-md-3">
+                            <button type="button" class="close" data-dismiss="alert">
+                                <i class="fa fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+</div>
