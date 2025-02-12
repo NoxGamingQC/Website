dfdbcc7b9b2943fe602461236e53ddb9dbbb1f18
NoxGamingQC <noxgamingqc@gmail.com>
2025-02-11 19:28:06 -0500

    Add error number

--- resources/views/view/pos/maintenance.blade.php
+++ resources/views/view/pos/maintenance.blade.php
@@ -3,8 +3,8 @@
 
 <div class="text-center" style="padding-top:20%;padding-bottom:30%;background-color:#101010;">
     <img src="/img/Projects/lrc-filiale101.png">
-    <h1 style="color:#FFF">EN MAINTENANCE</h1>
-    <h3 style="color:#FFF">Le PDV est présentement sous maintenance. Désolé de ce contretemps.</h3>
+    <h1 style="color:#FFF">Erreur 503 - EN MAINTENANCE</h1>
+    <h3 style="color:#FFF">Le PDV est présentement en maintenance. Désolé de ce contretemps.</h3>
     <h3 style="color:#FFF">Il sera disponible sous peu.</h3>
     <hr />
     <h3 style="color:#FFF">Maintenu par Cde Jimmy Béland-Bédard - 819-852-8705</h3>
