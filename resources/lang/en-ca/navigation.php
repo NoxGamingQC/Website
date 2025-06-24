f5bd459b6cd1540bf4186866b219a61814536ec8
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-06-24 06:31:41 -0400

    Fix navigation

--- resources/lang/en-ca/navigation.php
+++ resources/lang/en-ca/navigation.php
@@ -4,5 +4,11 @@ return [
     'welcome' => "Welcome",
     'about_us' => "About us",
     'contact_us' => "Contact us",
+    'game_list' => "Game list",
+    'partners' => "Partners",
+    'projects' => "Projects",
+    'teams' => "Teams",
+    'twitch' => "Twitch",
+    'youtube' => "YouTube",
     'store' => "Store"
 ];
