e6a3331ea7c8d91ead0f3b0d2d441d62793de3fe
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-06-24 06:41:31 -0400

    Fix game list

--- resources/views/view/about_us/partners.blade.php
+++ resources/views/view/about_us/partners.blade.php
@@ -4,22 +4,22 @@
 
 <div class="container section">
     <div class="row">
-        <div class="col-md-12 item-content">
+        <div class="col-12 item-content">
             <div class="row">
-                <div class="col-md-12" id="title">
+                <div class="col-12" id="title">
                     <h2>{{trans('general.partners')}}</h2>
                     <hr />
                 </div>
-                <div class="col-md-12">
+                <div class="col-12">
                     <div class="row">
-                        <div class="col-md-offset-1 col-md-5">
+                        <div class="offset-1 col-5">
                             <div class="panel panel-default text-center">
                                 <div class="panel-body" style="background-image: url('/img/Partners/CorpsRoyaux.png') !important; background-size: cover !important; height: 200px !important;background-position:center center">
                                     <h3 class="text-badge"><b>Corps Royaux</b></h3>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-5">
                             <div class="panel panel-default text-center">
                                 <div class="panel-body" style="background-image: url('/img/Partners/Instant-Gaming.png') !important; background-size: cover !important; height: 200px !important;background-position:center center">
                                     <h3 class="text-badge"><b>Instant Gaming</b></h3>
@@ -33,22 +33,22 @@
     </div>
 </div>
 <div class="container section">
-    <div class="col-md-12">
+    <div class="col-12">
         <div class="row">
-            <div class="col-md-12" id="title">
+            <div class="col-12" id="title">
                 <h2>{{trans('general.affiliates')}}</h2>
                 <hr />
             </div>
-            <div class="col-md-12">
+            <div class="col-12">
                 <div class="row">
-                    <div class="col-md-5 col-md-offset-1">
+                    <div class="col-5 offset-1">
                         <div class="panel panel-default text-center">
                             <div class="panel-body extra-life-logo" style="height: 200px !important; background-position:center center">
                                 <h3 class="text-badge"><b>Extra Life</b></h3>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-5">
+                    <div class="col-5">
                         <div class="panel panel-default text-center">
                             <div class="panel-body img" style="background-image: url('/img/Affiliates/HumbleBundle.png') !important; background-size: cover !important; height: 200px !important; background-position:center center">
                                 <h3 class="text-badge"><b>Humble Bundle</b></h3>
