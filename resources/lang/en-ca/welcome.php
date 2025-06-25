394bb9fe318a1d489c7767e110854582bfb73a3b
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2025-06-25 12:28:40 -0400

    Update welcome.php

--- resources/lang/en-ca/welcome.php
+++ resources/lang/en-ca/welcome.php
@@ -4,6 +4,6 @@ return [
     'name'=> "Hi, my name is Jimmy Béland-Bédard",
     'slogan' => "Québécois, programmer, streamer, Tech enthusiast.",
     'gamertag' => "Also known as NoxGamingQC.",
-    'having_fun' => 'Currently having fun with <span class="badge text-bg-primary">PHP</span> <span class="badge text-bg-primary">JavaScript</span>',
-    'has_knowledge' => 'Has knowledge with <span class="badge text-bg-primary">C</span> <span class="badge text-bg-primary">C++</span> <span class="badge text-bg-primary">C#</span> <span class="badge text-bg-primary">Kotlin</span> <span class="badge text-bg-primary">PHP</span> <span class="badge text-bg-primary">JavaScript</span>'
+    'having_fun' => 'Currently having fun with <span class="badge text-bg-primary">JavaScript</span> <span class="badge text-bg-primary">PHP</span>',
+    'has_knowledge' => 'Has knowledge with <span class="badge text-bg-primary">C</span> <span class="badge text-bg-primary">C++</span> <span class="badge text-bg-primary">C#</span> <span class="badge text-bg-primary">JavaScript</span> <span class="badge text-bg-primary">Kotlin</span> <span class="badge text-bg-primary">PHP</span>'
 ];
