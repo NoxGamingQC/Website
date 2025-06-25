44704d1520434d5a063f15c8f7d3253a88658a21
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-06-25 19:14:00 -0400

    Editing projects

--- resources/views/view/projects.blade.php
+++ resources/views/view/projects.blade.php
@@ -5,32 +5,32 @@
 
 <div class="container py-5">
     <div class="row">
-        <div class="col-12">
-            <div class="row">
-                <div class="col-6 text-left">
-                    <br />
-                    <br />
-                    <h4>Our website</h4>
-                    <br />
-                    <p>{{trans('projects.website_description')}} <a class="btn btn-primary" href="https://www.noxgamingqc.ca">Visit</a></p>
-                </div>
-                <div class="col-6">
-                    <img class="text-center" src="/img/Projects/website.png" width="100%" />
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6" style="background-image: url('/img/Projects/website.png');background-size: cover;background-position: center; border-radius:10px;border: 3px solid rgba(255, 255, 255, 0.54);"></div>
+                        <div class="col-6">
+                            <h5 class="card-title">{{trans('projects.website')}}</h5>
+                            <p class="card-text">{{trans('projects.website_description')}}</p>
+                            <a href="#" class="btn btn-primary disabled">{{trans('general.not_available')}}</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-12 bg-dark content-item">
-            <div class="row">
-                <div class="col-6">
-                    <img class="text-center" src="/img/Projects/NoxBOT.png" width="250px" />
-                </div>
-                <div class="col-6 text-right">
-                    <br />
-                    <br />
-                    <br />
-                    <h4>NoxBOT</h4>
-                    <br />
-                    <p><a class="btn btn-primary" href="https://github.noxgamingqc.ca/NoxBOT">Visit</a>  {{trans('projects.noxbot_description')}}</p>
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6"  style="background-image: url('/img/Projects/NoxBOT.png');background-size: cover;background-position: center; border-radius:10px;border: 3px solid rgba(255, 255, 255, 0.54);">
+                        </div>
+                        <div class="col-6">
+                            <h5 class="card-title">{{trans('projects.noxbot')}}</h5>
+                            <p class="card-text">{{trans('projects.noxbot_description')}}</p>
+                            <a href="#" class="btn btn-primary">{{trans('general.visit')}}</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
