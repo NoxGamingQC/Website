a85a95025e2baccbdc1cabaa051bef9bc3c2bc4f
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2025-06-25 12:55:45 -0400

    Update welcome.blade.php

--- resources/views/view/welcome.blade.php
+++ resources/views/view/welcome.blade.php
@@ -6,9 +6,9 @@
     <div class="row py-5 align-items-center">
         <div class="col">
             <h2><b>{{trans('welcome.name')}}</b> <small class="text-body-secondary">{{trans('welcome.gamertag')}}</small></h2>
-            <h3>{{trans('welcome.slogan')}}</h3>
-            <h5>{!! trans('welcome.having_fun') !!}</h5>
-            <h5>{!! trans('welcome.has_knowledge') !!}</h5>
+            <h5>{{trans('welcome.slogan')}}</h5>
+            <h6>{!! trans('welcome.having_fun') !!}</h6>
+            <h6>{!! trans('welcome.has_knowledge') !!}</h6>
         </div>
     </div>
 </div>
