b31f48d6748dcdcbbeb2c79560793692c4798e3f
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-06-25 16:33:46 -0400

    Fix footer

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -1,15 +1,17 @@
 <div class="footer bg-primary py-5 text-bg-primary my-auto" style="max-height:30vh">
     <div class="container align-items-center">
         <div class="row">
-            <div class="col-4 text-center row justify-content-center align-self-center">
-                <span>Contact: <a class="text-white" href="mailto:jbedard@noxgamingqc.ca">jbedard@noxgamingqc.ca</a></span>
-                <span>Shawinigan, QC, Canada</span>
+            <div class="col-4 text-center">
+                <a class="text-white" href="mailto:jbedard@noxgamingqc.ca">jbedard@noxgamingqc.ca</a>
+                <br/>
+                <span>Québec, Canada</span>
+                <br/>
                 <a class="text-white" href="https://github.com/NoxGamingQC">GitHub</a>
             </div>
-            <div class="col-4 text-center row justify-content-center align-self-center">
-                <img src="/img/logo.png" alt="logo" style="width:150px">
+            <div class="col-4 text-center">
+                <img src="/img/logo.png" alt="logo" style="width:50%">
             </div>
-            <div class="col-4 text-center row justify-content-center align-self-center">
+            <div class="col-4 text-center">
                 <h4>{{trans('footer.build_using')}}</h4>
                 <a class="text-white" href="https://getbootstrap.com/">Bootstrap</a>
                 <a class="text-white" href="https://laravel.com/">Laravel</a>
