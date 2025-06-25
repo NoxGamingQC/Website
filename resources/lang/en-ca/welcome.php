845eb28276432b2430e23a1e7fcf5c286ed7dd7c
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-06-25 17:10:52 -0400

    Move project tab

--- resources/lang/en-ca/welcome.php
+++ resources/lang/en-ca/welcome.php
@@ -5,5 +5,5 @@ return [
     'slogan' => "Québécois, programmer, streamer, Tech enthusiast.",
     'gamertag' => 'Also known as <span class="text-primary">NoxGamingQC.</span>',
     'having_fun' => 'Currently having fun with <span class="badge text-bg-primary">JavaScript</span> <span class="badge text-bg-primary">PHP</span>',
-    'has_knowledge' => 'Has knowledge with <span class="badge text-bg-primary">C</span> <span class="badge text-bg-primary">C++</span> <span class="badge text-bg-primary">C#</span> <span class="badge text-bg-primary">JavaScript</span> <span class="badge text-bg-primary">Kotlin</span> <span class="badge text-bg-primary">PHP</span>'
+    'has_knowledge' => 'Has knowledge on <span class="badge text-bg-primary">C</span> <span class="badge text-bg-primary">C++</span> <span class="badge text-bg-primary">C#</span> <span class="badge text-bg-primary">JavaScript</span> <span class="badge text-bg-primary">Kotlin</span> <span class="badge text-bg-primary">PHP</span>'
 ];
