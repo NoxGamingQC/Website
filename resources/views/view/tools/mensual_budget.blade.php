438c700dc3a7eb9767cea33e20b816bec5b404cc
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-07-01 17:13:57 -0400

    Add budget tool

--- resources/views/view/tools/mensual_budget.blade.php
+++ resources/views/view/tools/mensual_budget.blade.php
@@ -6,14 +6,14 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-12">
-            <h1>{{trans('tools.mensual_budget')}}</h1>
+            <h1>{{trans('tools/mensual_budget.mensual_budget')}}</h1>
             <p class="no-print">For privacy reason, no data is or will be saved on this page. Upon refreshing the page all data will be lost. If you want to keep those data, you can print this page using <kbd>CTRL</kbd> + <kbd>P</kbd> or in your browser settings. All unnecessary text will disapear on the printed document.</p>
         </div>
     </div>
     <div class="col-6">
         <h4 class="text-success">Total income: <span id="totalIncome"></span></h4>
-        <h4 class="text-danger">Total expenses: </h4>
-        <h4 class="text-info">Total saved: </h4>
+        <h4 class="text-danger">Total expenses: <span id="totalExpense"></span></h4>
+        <h4 class="text-info">Total saved: <span id="totalSaved"></span></h4>
     </div>
     <div class="col-12">
         <table id="incomes" class="table table-success table-striped">
@@ -26,7 +26,7 @@
         </table>
     </div>
     <div class="col-md-12 no-print">
-        <table id="incomes" class="table table-success table-striped">
+        <table class="table table-success table-striped">
             <tr>
                 <th>Name</th>
                 <th>Amount</th>
@@ -59,6 +59,31 @@
             </tr>
         </table>
     </div>
+    <div class="col-md-12 no-print">
+        <table class="table table-danger table-striped">
+            <tr>
+                <th>Name</th>
+                <th>Amount</th>
+                <th>Frequency</th>
+                <th>Pay before</th>
+                <th class="no-print text-center"><i class="fa fa-plus" aria-hidden="true"></i></th>
+            </tr>
+            <tr>
+                <td><input id="expenseName" type="text" class="form-control" placeholder="Name or description of the expense" /></td>
+                <td class="input-group"><input id="expenseAmount" type="text" class="form-control" placeholder="0.00" /><span class="input-group-text">$</span></td>
+                <td>
+                    <select id="expenseFrequency" class="form-select">
+                        <option selected>Monthly</option>
+                        <option disabled>Bi-monthly</option>
+                        <option disabled>Weekly</option>
+                        <option disabled>Daily</option>
+                    </select>
+                </td>
+                <td><input id="expensePayBefore" type="text" class="form-control" placeholder="0" /></td>
+                <td class="text-center"><button id="addExpense" class="btn btn-success"><i class="fa fa-plus" aria-hidden="true"></i></button></td>
+            </tr>
+        </table>
+    </div>
 
 </div>
 <script type="text/javascript">
@@ -79,8 +104,23 @@
         refreshData();
     });
 
+    $('#addExpense').on('click', function() {
+        var html = 
+            '<tr>'+
+                '<td>'+ $('#expenseName').val() +'</td>'+
+                '<td class="expense-amount" value="'+ ($('#expenseAmount').val()).replace(',', '.') +'">'+ ($('#expenseAmount').val()).replace(',', '.') +' $</td>'+
+                '<td>'+ $('#expenseFrequency').val() +'</td>'+
+                '<td>'+ $('#expensePayBefore').val() +'</td>'+
+                '<td class="no-print text-center"><button class="btn btn-danger remove"><i class="fa fa-trash" aria-hidden="true"></i></button></td>'+
+            '</tr>';
+        var expenseTable = $('#expenses').html();
+        $('#expenses').html(expenseTable + html);
+        refreshData();
+    });
+
     function refreshData() {
         var totalIncome = 0;
+        var totalExpense = 0;
         $('.remove').on('click', function () {
             var row = $(this).parent().parent();
             row.remove();
@@ -92,7 +132,14 @@
             totalIncome += Number(income.attr('value'));
         })
 
-        $('#totalIncome').html(totalIncome + ' $')
+        $('.expense-amount').each(function() {
+            var expense = $(this)
+            totalExpense += Number(expense.attr('value'));
+        })
+
+        $('#totalIncome').html(totalIncome.toFixed(2) + ' $')
+        $('#totalExpense').html(totalExpense.toFixed(2) + ' $')
+        $('#totalSaved').html(Number(totalIncome.toFixed(2) - totalExpense) + ' $')
         
     }
 
