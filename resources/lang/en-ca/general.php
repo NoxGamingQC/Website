3e5bbd4cd07b3ac8c4f3d3bcf30f21c23616b528
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-08-17 12:44:23 -0400

    sign in and up button in nav

--- resources/lang/en-ca/general.php
+++ resources/lang/en-ca/general.php
@@ -5,5 +5,12 @@ return [
     'visit_source' => "Sources",
     'github_pages' => "GitHub pages",
     'in_development' => "In development",
-    'website' => "Site web"
+    'website' => "Site web",
+
+    //auth
+    'email_address' => "Email",
+    'password' => "Password",
+    'remember_me' => "Remember me",
+    'forgot_password' => 'Forgot password',
+    'login' => "Sign in"
 ];
