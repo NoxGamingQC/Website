faf4492bd25b534b77360ca00d4c225c311f716f
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-10-16 08:03:04 -0400

    Fix parsing on mobile

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -16,7 +16,7 @@
                 <div class="col-12">
                     <div class="row">
                         @foreach($feed->get_items() as $item)
-                            <div class="col-3 py-2">
+                            <div class="col-md-3 py-2">
                                 <div class="card">
                                     <img src="{{ preg_match('/<img.+src=[\'"](?P<src>.+?)[\'"].*>/i', $item->get_description(), $image) ? $image['src'] : '/img/no-image.png' }}" class="card-img-top" alt="..." max-height="200px" style="object-fit: cover; height: 200px;">
                                     <div class="card-body">
