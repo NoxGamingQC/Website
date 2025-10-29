fe45173fa109ff1f563242351d642a1e586bd340
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-10-28 20:37:44 -0400

    Edit title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
         <meta name="author" content="NoxGamingQC">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{$appName}} - @yield('title')</title>
+        <title>@yield('title')</title>
         <link rel="icon" href="/img/logo.png" type="image/png">
         <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
         <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">-->
