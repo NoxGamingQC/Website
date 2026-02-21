15afbed0dff6273d39ae9c98d45380a73ce749cc
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2026-02-20 19:57:37 -0500

    Fix mail

--- app/Model/MailAttachment.php
+++ app/Model/MailAttachment.php
@@ -4,9 +4,9 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
-class MailIndex extends Model
+class MailAttachment extends Model
 {
-    protected $table = 'mail_index';
+    protected $table = 'mail_attachments';
 
     const CREATED_AT = 'created_at';
     const UPDATED_AT = 'updated_at';
---
+++ app/Model/MailRecord.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MailRecord extends Model
+{
+    protected $table = 'mail_records';
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+}
