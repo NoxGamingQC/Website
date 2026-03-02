9d46f674a72c1ad926854eda616b0eb5f0e92748
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2026-03-01 21:21:27 -0500

    Redo welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,17 @@
 <div class="container text-justify my-auto">
     <div class="row py-5 align-items-center">
         <div class="col card card-body">
-            <h2>
-                <b class="display-4">{!! trans('welcome.name') !!}</b>
+            <h2 class="display-4 font-weight-bold">
+                {!! trans('welcome.name') !!}
                 <br />
                 <small>{!! trans('welcome.gamertag') !!}</small>
             </h2>
-            <h5>{{trans('welcome.slogan')}}</h5>
+
+            <h5>{{ trans('welcome.slogan') }}</h5>
             <h6>{!! trans('welcome.having_fun') !!}</h6>
             <h6>{!! trans('welcome.has_knowledge') !!}</h6>
         </div>
     </div>
 </div>
+
 @endsection
