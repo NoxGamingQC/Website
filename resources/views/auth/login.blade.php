f5b75463723482f3a832cf4a6dacd692afe1142e
Jimmy Béland-Bédard <18665917+NoxGamingQC@users.noreply.github.com>
2026-04-07 16:18:05 -0400

    Fix login page style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,20 +20,18 @@
                                 <span style="color:#252525">{{trans('auth.failed')}}</span>
                             </div>
                             <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                <label for="name" class="control-label">{{trans('auth.name')}}</label>
-                                    <div class="input-group mb-3">
-                                        <span class="input-group-text"><i class="fa fa-user" aria-hidden="true"></i></span>
-                                        <input id="name" type="name" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                    <div class="custom-input-group mb-3">
+                                        <span class="text-black icon-addon"><i class="fa fa-user" aria-hidden="true"></i></span>
+                                        <input id="name" type="name" class="form-control" name="name" value="{{ old('name') }}" placeholder="{{ trans('auth.name') }}" required autofocus>
                                     </div>
                             </div>
                             <br />
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="control-label">{{trans('general.password')}}</label>
-                                <div class="input-group mb-3">
-                                    <span class="input-group-text"><i class="fa fa-lock" aria-hidden="true"></i></span>
-                                    <input id="password" type="password" class="form-control" name="password" required>
+                                <div class="custom-input-group mb-3">
+                                    <span class="text-black icon-addon"><i class="fa fa-lock" aria-hidden="true"></i></span>
+                                    <input id="password" type="password" class="form-control" name="password" placeholder="{{ trans('general.password') }}" required>
                                 </div>
-                                <span class="text-body-secondary">{{trans('auth.credentials_case_sensitive')}}</span>
+                                <span class="text-warning">{{trans('auth.credentials_case_sensitive')}}</span>
                             </div>
                         </div>
                     </div>
@@ -42,7 +40,7 @@
                 <div class="col-3 offset-3">
                     <div class="checkbox">
                         <label>
-                            <input type="checkbox" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}> {{trans('general.remember_me')}}
+                            <input type="checkbox" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}> <span class="form-check-label">{{trans('general.remember_me')}}</span>
                         </label>
                     </div>
                 </div>
