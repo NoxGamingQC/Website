38a85bdf11568aad3fd8bcb12c0491e253a57294
Jimmy Béland-Bédard <18665917+NoxGamingQC@users.noreply.github.com>
2026-04-07 11:24:11 -0400

    Edit style

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,25 +1,9 @@
 <footer class="footer no-print py-4">
     <div class="container">
         <div class="row text-center align-items-center">
-            <div class="col-4">
-                <a href="mailto:jbedard@noxgamingqc.ca">jbedard@noxgamingqc.ca</a>
-                <br />
-                <span>Québec, Canada</span>
-                <br />
-                <a href="https://github.com/NoxGamingQC">GitHub</a>
+            <div class="col-12">
+                © 2023-{{ date('Y') }} Jimmy Béland-Bédard — Tous droits réservés.
             </div>
-            <div class="col-4">
-                <img src="/img/logo.png" alt="logo" style="width:50%">
-            </div>
-            <div class="col-4">
-                <h4>{{trans('footer.build_using')}}</h4>
-                <a href="https://getbootstrap.com/">Bootstrap</a>
-                <br />
-                <a href="https://laravel.com/">Laravel</a>
-            </div>
-            <!--<div class="col-12">
-                <p class=""><small>commit: {$sourceVersion}}<small></p>
-            </div>-->
         </div>
     </div>
 </footer>
